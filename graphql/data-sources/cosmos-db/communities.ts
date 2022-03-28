827577b815ee391ccffe9afe8383da5edfe84bd9
Patrick Gidich <patrick@simnova.com>
2022-03-28 17:29:12 -0400

    fixed pipleine

--- graphql/data-sources/cosmos-db/communities.ts
+++ graphql/data-sources/cosmos-db/communities.ts
@@ -1,7 +1,8 @@
 import { msRestAzureVersion } from '@azure/ms-rest-azure-js';
 import { MongoDataSource } from 'apollo-datasource-mongodb';
 import { resourceLimits } from 'worker_threads';
-import { Community } from '../../../infrastructure/data-sources/cosmos-db/models/community';
+import { Community, CommunityModel } from '../../../infrastructure/data-sources/cosmos-db/models/community';
+import { UserModel } from '../../../infrastructure/data-sources/cosmos-db/models/user';
 import { Context } from '../../context';
 
 export class Communities extends MongoDataSource<Community, Context> {
@@ -26,56 +27,50 @@ export class Communities extends MongoDataSource<Community, Context> {
       })?.[0];
   }
   async getCommunitiesForCurrentUser(): Promise<Community[]> {
-    var externalId = this.context.verifiedUser.verifiedJWT.sid
-    var result = await this.collection.aggregate(
-      [
-        { 
-          "$project" : { 
-              "_id" : 0, 
-              "community" : "$$ROOT"
+    var externalId = this.context.verifiedUser.verifiedJWT.sub
+    // starts from user (looking up by externalId), then find where they are a member, and then find the communities they are a member of
+    var result = await UserModel.aggregate<Community>(
+    [
+      { 
+          "$match" : { 
+              "externalId" : externalId
           }
       }, 
-        { 
-            "$lookup" : { 
-                "localField" : "community.id", 
-                "from" : "members", 
-                "foreignField" : "id", 
-                "as" : "member"
-            }
-        }, 
-        { 
-            "$unwind" : { 
-                "path" : "$member", 
-                "preserveNullAndEmptyArrays" : false
-            }
-        }, 
-        { 
-            "$lookup" : { 
-                "localField" : "member.accounts.user", 
-                "from" : "users", 
-                "foreignField" : "id", 
-                "as" : "user"
-            }
-        }, 
-        { 
-            "$unwind" : { 
-                "path" : "$user", 
-                "preserveNullAndEmptyArrays" : false
-            }
-        }, 
-        { 
-            "$match" : { 
-                "user.externalId" : externalId
-            }
-        }, 
-
-    ],
-    { 
-        "allowDiskUse" : true
-    }
-    ).toArray();
-    
-    return result as Community[];
+      { 
+          "$lookup" : { 
+              "from" : "members", 
+              "localField" : "_id", 
+              "foreignField" : "accounts.user", 
+              "as" : "m"
+          }
+      }, 
+      { 
+          "$match" : { 
+              "m" : { 
+                  "$ne" : []
+              }
+          }
+      }, 
+      { 
+          "$lookup" : { 
+              "from" : "communities", 
+              "localField" : "m.community", 
+              "foreignField" : "_id", 
+              "as" : "c"
+          }
+      }, 
+      { 
+          "$unwind" : { 
+              "path" : "$c", 
+              "preserveNullAndEmptyArrays" : true
+          }
+      }, 
+      { 
+          "$replaceWith" : "$c"
+      }
+    ]
+    ).exec();
+    return result.map(r =>  CommunityModel.hydrate(r));
   }
   
 

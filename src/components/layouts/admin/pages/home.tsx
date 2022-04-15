e1ef519b7bed94efd218c9d20dd407fed21bc9c0
clsscrch <richardli121499@gmail.com>
2022-04-15 09:55:27 -0400

    Added a basic home page, replacing the "home" text

--- src/components/layouts/admin/pages/home.tsx
+++ src/components/layouts/admin/pages/home.tsx
@@ -1,7 +1,21 @@
+import {PageHeader} from 'antd'
+import { SubPageLayout } from '../sub-page-layout'
+
 export const Home: React.FC<any> = (props) => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    // <div>
+    //   <h1>Home</h1>
+    // </div>
+
+    <SubPageLayout
+    fixedHeader={false}
+    header={
+      <PageHeader 
+        title="Home"
+      />}
+    >
+
+    </SubPageLayout>
+    
   )
 }

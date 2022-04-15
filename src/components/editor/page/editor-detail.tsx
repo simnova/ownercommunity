169142bef3148eb5a3902b6f65d894344b68a601
nnoce14 <nnoce12@gmail.com>
2022-04-15 09:25:06 -0400

    Fixed typo in save popup message

--- src/components/editor/page/editor-detail.tsx
+++ src/components/editor/page/editor-detail.tsx
@@ -33,7 +33,7 @@ export const EditorDetail = (props:any) => {
     setPageLayouts(pageLayouts);
     notification.success({
       message: "Saved",
-      description: "Saaved Successfully"
+      description: "Saved Successfully"
     });
   }
 

9612e6c548f2c026d376e81baa1ab5bfc0d44062
clsscrch <richardli121499@gmail.com>
2022-04-18 09:29:52 -0400

    Changed the homepage

--- src/components/layouts/admin/pages/home.tsx
+++ src/components/layouts/admin/pages/home.tsx
@@ -1,7 +1,14 @@
-import {PageHeader} from 'antd'
+import { UsernamePasswordClient } from '@azure/msal-common'
+import {PageHeader, Typography} from 'antd'
+import { useParams } from 'react-router-dom'
 import { SubPageLayout } from '../sub-page-layout'
 
+const { Text } = Typography
+
+
 export const Home: React.FC<any> = (props) => {
+  const params = useParams();
+  console.log(params);
   return (
     // <div>
     //   <h1>Home</h1>
@@ -14,7 +21,8 @@ export const Home: React.FC<any> = (props) => {
         title="Home"
       />}
     >
-
+      <Text> Community ID: {params.communityId}</Text>
+      
     </SubPageLayout>
     
   )

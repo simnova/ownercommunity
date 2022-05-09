c9bef3860064500109aae15acc219f672875300f
clsscrch <richardli121499@gmail.com>
2022-05-09 14:10:26 -0400

    Renders white label, domain, and handle names on the homepage

--- src/components/layouts/admin/components/community-detail.tsx
+++ src/components/layouts/admin/components/community-detail.tsx
@@ -3,6 +3,45 @@ import { Typography, Descriptions } from 'antd';
 const { Text } = Typography;
 
 export const CommunityDetail: React.FC<any> = (props) => {
+
+  const whiteLabel = () => {
+    if (props.data.whiteLabelDomain){
+      return (
+      <Descriptions.Item label="White Label Name">
+        <Text strong>{props.data.whiteLabelDomain}</Text>
+      </Descriptions.Item>
+      )
+    } else {
+      return <></>
+    }
+  }
+
+  const hasDomain = () => {
+    if (props.data.domain) {
+      return (
+        <Descriptions.Item label="Domain Name">
+          <Text strong>{props.data.domain}</Text>
+        </Descriptions.Item>
+        )
+    }
+  }
+
+  const hasHandle = () => {
+    if (props.data.handle){
+      return (
+      <Descriptions.Item label="Handle Name">
+        <Text strong>{props.data.handle}</Text>
+      </Descriptions.Item>
+      )
+    } else {
+      return <></>
+    }
+  }
+
+
+
+  
+
   return (
     <Descriptions column={1}>
       <Descriptions.Item label="Community ID">
@@ -11,6 +50,9 @@ export const CommunityDetail: React.FC<any> = (props) => {
       <Descriptions.Item label="Community Name">
         <Text strong>{props.data.name}</Text>
       </Descriptions.Item>
+      {whiteLabel()}
+      {hasDomain()}
+      {hasHandle()}
     </Descriptions>
   );
 };

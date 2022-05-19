1082c43082144322485f77d3def4b4f2e5649508
clsscrch <richardli121499@gmail.com>
2022-05-19 10:47:00 -0400

    Need to implement community property details

--- src/components/layouts/members/pages/community-property-detail.tsx
+++ src/components/layouts/members/pages/community-property-detail.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useNavigationType, useParams } from "react-router-dom";
 import { PageHeader, Typography } from 'antd';
 import { SubPageLayout } from '../sub-page-layout';
 
@@ -8,15 +8,18 @@ const { Text } = Typography;
 
 export const CommunityPropertyDetail: React.FC<any> = (props) => {
     const params = useParams();
+    const navigate = useNavigate();
     return (
       <SubPageLayout
       fixedHeader={false}
       header={
         <PageHeader 
           title="Property Details"
+          onBack={() => navigate(-1)}
         />}
       >
         <Text> Property ID: {params.propertyId}</Text>
+        <Text> Implement me!</Text>
 
       </SubPageLayout>
       

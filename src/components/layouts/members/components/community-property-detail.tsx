0bb5da9827c1dbf9c567005f3a2967bdee541a24
clsscrch <richardli121499@gmail.com>
2022-05-19 16:32:56 -0400

    Added some information for the property details page

--- src/components/layouts/members/components/community-property-detail.tsx
+++ src/components/layouts/members/components/community-property-detail.tsx
@@ -1,16 +1,16 @@
-import { Image } from 'antd';
+import { Image, Typography, Space } from 'antd';
+const { Title, Text  } = Typography;
 
 export const CommunityPropertyDetail: React.FC<any> = (props) => {
     console.log(props.data.property)
 
     const generateMarketData = () => {
         return (
-            <div>
-                {props.data.property.listedForSale && <div>
-
+            <Space>
+                {props.data.property.listedForSale && <Title level={3}>
                     {props.data.property.listingDetail.price}
-                </div>
-                
+                </Title>
+
                 }
 
                 {props.data.property.listedForRent && <div></div>
@@ -20,7 +20,16 @@ export const CommunityPropertyDetail: React.FC<any> = (props) => {
                 {props.data.property.listedForLease && <div></div>
 
                 }
-        </div>
+
+                <Space>
+                    <Text>{props.data.property.listingDetail.bedrooms ? props.data.property.listingDetail.bedrooms : "-"} Bds</Text>
+
+                </Space>
+
+
+
+                
+            </Space>
         )
     }
 
@@ -29,7 +38,12 @@ export const CommunityPropertyDetail: React.FC<any> = (props) => {
             <Image 
                 src='https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
                 height='20rem'
+                width="60rem"
             />
+            <Title level={3}>123 Street St</Title>
+            <Title level={2} style={{marginTop: '15px'}}>{props.data.property.propertyName}</Title>
+            {/* <Title level={3}>{props.data.property.location}</Title> */}
+
             {generateMarketData()}
         </div>
     )

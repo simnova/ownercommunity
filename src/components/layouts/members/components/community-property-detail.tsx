1380f3d3773f0a3a72039da28ecbbb15025ab3d3
clsscrch <richardli121499@gmail.com>
2022-05-20 12:02:55 -0400

    Added more details to detail page

--- src/components/layouts/members/components/community-property-detail.tsx
+++ src/components/layouts/members/components/community-property-detail.tsx
@@ -1,4 +1,5 @@
-import { Image, Typography, Space } from 'antd';
+import { Image, Typography, Space, Divider } from 'antd';
+import { TextComponent } from '../../../editor/components';
 const { Title, Text  } = Typography;
 
 export const CommunityPropertyDetail: React.FC<any> = (props) => {
@@ -6,25 +7,28 @@ export const CommunityPropertyDetail: React.FC<any> = (props) => {
 
     const generateMarketData = () => {
         return (
-            <Space>
-                {props.data.property.listedForSale && <Title level={3}>
-                    {props.data.property.listingDetail.price}
+            <Space direction='vertical'>
+                {props.data.property.listedForSale && 
+                <Title level={3}>
+                    Sale Price: ${props.data.property.listingDetail.price}
                 </Title>
 
                 }
 
-                {props.data.property.listedForRent && <div></div>
+                {props.data.property.listedForRent && 
+                <Title level={3}>
+                    Rent: ${props.data.property.listingDetail.rentLow} - ${props.data.property.listingDetail.rentHigh}
+                </Title>
+                }
+
+                {props.data.property.listedForLease && 
+                <Title level={3}>
+                    Lease: ${props.data.property.listingDetail.lease}
+                </Title>
 
                 }
 
-                {props.data.property.listedForLease && <div></div>
-
-                }
-
-                <Space>
-                    <Text>{props.data.property.listingDetail.bedrooms ? props.data.property.listingDetail.bedrooms : "-"} Bds</Text>
-
-                </Space>
+               
 
 
 
@@ -33,19 +37,46 @@ export const CommunityPropertyDetail: React.FC<any> = (props) => {
         )
     }
 
+    const generateAgentDetails = () => {
+        return (
+            
+            <Space direction='vertical'>
+                <Divider orientation='left' orientationMargin={"5px"}><Title level={5}>Agent Details</Title></Divider>
+                <Space>
+                    {props.data.property.listingDetail.listingAgent}
+                    <Text italic style={{color: 'gray'}}>{props.data.property.listingDetail.listingAgentCompany}</Text>
+                </Space>
+                {props.data.property.listingDetail.listingAgentEmail}
+                {props.data.property.listingDetail.listingAgentPhone}
+                {props.data.property.listingDetail.listingAgentWebsite}
+            </Space>
+        )
+    }
+
     return (
-        <div>
-            <Image 
-                src='https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                height='20rem'
-                width="60rem"
-            />
-            <Title level={3}>123 Street St</Title>
-            <Title level={2} style={{marginTop: '15px'}}>{props.data.property.propertyName}</Title>
+        <Space direction='vertical'>
+            <Space direction='vertical' size={0}>
+                <Title level={2} style={{marginBottom: "0px"}}>{props.data.property.propertyName}</Title>
+                <Text italic style={{color: "gray"}}>Owned By: {props.data.property.owner.memberName}</Text>
+            </Space>
+
+            <Space direction='horizontal' size={50} >
+                <Title level={3} style={{marginTop: '0px'}}>123 Street St</Title>
+                <Title level={4}>{props.data.property.listingDetail.bedrooms ? props.data.property.listingDetail.bedrooms : "-"} Bds</Title>
+                <Title level={4}>{props.data.property.listingDetail.bathrooms ? props.data.property.listingDetail.bathrooms : "-"} Ba</Title>
+                <Title level={4}>{props.data.property.listingDetail.squareFeet ? props.data.property.listingDetail.squareFeet : "-"} Sqft</Title>
+            </Space>
+            
+
+
             {/* <Title level={3}>{props.data.property.location}</Title> */}
 
             {generateMarketData()}
-        </div>
+
+            <Divider orientation='left' orientationMargin={"5px"}><Title level={5}>About 123 Street St</Title></Divider>
+            <Text italic>{props.data.property.listingDetail.description}</Text>
+            {generateAgentDetails()}
+        </Space>
     )
 
 }

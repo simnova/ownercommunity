305e40b32ad3210e787f1c87396e1649534d0412
Patrick Gidich <patrick.gidich@simnova.com>
2022-05-22 15:05:30 -0400

    formating file list

--- src/components/layouts/admin/components/site-editor-files-list.tsx
+++ src/components/layouts/admin/components/site-editor-files-list.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Table, TableColumnsType, Row, message, Button } from 'antd';
+import { Table, TableColumnsType, Row, message, Button, notification } from 'antd';
 import { FileInfo } from '../../../../generated';
 import ResizeObserver from 'rc-resize-observer';
 import './site-editor-files-list.css';
+import copy from "copy-to-clipboard";
+import { LinkOutlined } from '@ant-design/icons';
 
 export interface SiteEditorFilesListProps {
   data: FileInfo[];
@@ -12,27 +14,57 @@ export interface SiteEditorFilesListProps {
 export const SiteEditorFilesList: React.FC<SiteEditorFilesListProps> = (props) => {
   const [tableHeight, setTableHeight] = useState(0);
   //const scroll = true;
+
+  const bytesToSize = (bytes: number): string => {
+    const sizes: string[] = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (bytes === 0) return 'n/a'
+    const i: number = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString())
+    if (i === 0) return `${bytes} ${sizes[i]}`
+    return `${(bytes / Math.pow(1024, i)).toFixed(1)} ${sizes[i]}`
+  }
+
+  const mimeTypeToFileType = (mimeType: string): string => {
+    if (mimeType === 'text/plain') {
+      return 'TEXT';
+    } else if (mimeType.startsWith('image/') || mimeType.startsWith('application/') || mimeType.startsWith('text/')) {
+      return mimeType.substring(mimeType.indexOf('/') + 1).toUpperCase();
+    } else {
+      return 'file';
+    }
+  }
+
+  const copyUrl = (url:string) => {
+    copy(url);
+    notification.success({
+      message: "Copied to Clipboard",
+      description: "The link has been copied to your clipboard"
+    });
+  }
   
   const columns:TableColumnsType<FileInfo> = [
     {
-      title: "name",
+      title: "File",
       dataIndex: "name",
       key: "name",
+      render: (text: any) => text ? <span>{text.substring(text.indexOf('/')+1)}</span> : <span>n/a</span>
     },
     {
-      title: "size",
+      title: "Size",
       dataIndex: "size",
       key: "size",
+      render: (text: any) => text ? <span>{bytesToSize(text)}</span> : <span>n/a</span>
     },
     {
-      title: "type",
+      title: "Type",
       dataIndex: "type",
       key: "type",
+      render: (text: any) => text ? <span>{mimeTypeToFileType(text)}</span> : <span>n/a</span>
     },
     {
-      title: "url",
+      title: "Link",
       dataIndex: "url",
       key: "url",
+      render: (text: any) => text ? <Button icon={<LinkOutlined />} onClick={()=> copyUrl(text)}></Button> : <span>n/a</span>
     },
   ]
 

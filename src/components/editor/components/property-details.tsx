d96ae1265a3498f403b446ccdced3c3774af222d
nnoce14 <nnoce12@gmail.com>
2022-05-24 14:47:04 -0400

    Added CommunityPropertyDetail to cms component

--- src/components/editor/components/property-details.tsx
+++ src/components/editor/components/property-details.tsx
@@ -3,6 +3,7 @@ import { gql, useQuery } from '@apollo/client';
 import { useNode } from '@craftjs/core';
 import { MemberPropertyByPropertyIdDocument } from '../../../generated';
 import { Typography, Card, Space, Badge, Skeleton } from 'antd';
+import { CommunityPropertyDetail } from '../../layouts/members/components/community-property-detail';
 
 let PropertyDetails: any;
 
@@ -16,21 +17,21 @@ PropertyDetails = () => {
         }
     ));
 
-    const { loading, error, data } = useQuery(MemberPropertyByPropertyIdDocument, {
+    const { loading: propertyLoading, error: propertyError, data: propertyData } = useQuery(MemberPropertyByPropertyIdDocument, {
       variables: { propertyId: propertyId}
     }
     );
 
     const content = () => {
-        if (loading) return <Skeleton active/>;
-        if (error) return <p>Error! ${error.message}</p>;
-        if (data) return (
+        if (propertyLoading) return <Skeleton active/>;
+        if (propertyError) return <p>Error! ${propertyError.message}</p>;
+        if (propertyData) return (
             <div 
                 className="px-4 py-2"
                 ref={ref => connect(drag(ref as HTMLDivElement))} 
             >
-                <div className="bg-white shadow overflow-hidden sm:rounded" style={{display:'flex', justifyContent: 'space-around', flexWrap: 'wrap'}}>
-                    Property Details go here
+                <div className="bg-white shadow overflow-hidden sm:rounded">
+                    <CommunityPropertyDetail data={propertyData} />
                 </div>
             </div>
         )

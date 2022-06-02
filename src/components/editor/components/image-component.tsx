c3415fd442cad11ff9b65969c8290f67f456f0b0
nnoce14 <nnoce12@gmail.com>
2022-06-02 11:44:42 -0400

    Replaced text dropdown with modal that previews the images in a community

--- src/components/editor/components/image-component.tsx
+++ src/components/editor/components/image-component.tsx
@@ -1,7 +1,7 @@
 import { useNode } from '@craftjs/core'
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
-import { Form, Image, Skeleton, Select, Row, Col, Slider, InputNumber } from 'antd'
+import { Form, Image, Skeleton, Select, Row, Col, Slider, InputNumber, Modal, Button } from 'antd'
 import { gql, useQuery } from '@apollo/client';
 
 const { Option } = Select;
@@ -33,6 +33,10 @@ ImageComponent = ({ src, width }: ImageProp) => {
     <div
       className="px-4 py-2"
       ref={ref => connect(drag(ref as HTMLDivElement))}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
     >
         <Image src={src} fallback="https://joeschmoe.io/api/v1/random" preview={false} width={width} />
     </div>
@@ -41,14 +45,13 @@ ImageComponent = ({ src, width }: ImageProp) => {
 
 var ImageComponentSettings = () => {
   const params = useParams();
-  const [inputValue, setInputValue] = useState(1);
+  const [isModalVisible, setModalVisible] = useState<boolean>(false);
+  const [imageUrl, setImageUrl] = useState<string>("");
 
   const { actions: { setProp}, src, width } = useNode((node) => ({
     src: node.data.props.src,
     width: node.data.props.width,
   }));
-
-  console.log("COMMUNITY ID: ", params.communityId);
 
   const { data, loading, error } = useQuery(GET_IMAGES_IN_COMMUNITY, {
     variables: { communityId: params.communityId}
@@ -64,12 +67,33 @@ var ImageComponentSettings = () => {
     return (
       <div>
         <Form layout="vertical">
-          <Form.Item label="Source File">
-            <Select defaultValue={src === '' ? "Select an image" : src} onChange={(inputElement) => setProp((props: any) => props.src = inputElement)}>
+          <Form.Item>
+            <Button type='primary' onClick={() => {
+              setImageUrl(src);
+              setModalVisible(true);
+            }} >
+              Select an image
+            </Button>
+            <Modal 
+              title="Select an image" 
+              visible={isModalVisible} 
+              onOk={() => { 
+                setProp((props: any) => props.src = imageUrl); 
+                setModalVisible(false)
+              }} 
+              onCancel={() => setModalVisible(false)}
+            >
               {data.communityById.filesByType.map((file: any) => (
-                <Option value={file.url}>{file.name.slice(13)}</Option>
+                  <Image 
+                    src={file.url} 
+                    fallback="https://joeschmoe.io/api/v1/random" 
+                    preview={false} 
+                    onClick={(e: any) => setImageUrl(e.target.src)} 
+                    className={imageUrl === file.url ? 'selected-img' : ''}
+                    style={{ margin: '0 5px', maxWidth: '100px', minWidth: '32px' }}
+                  />
               ))}
-            </Select>
+            </Modal>
           </Form.Item>
           <Form.Item label="Width">
             <Row>

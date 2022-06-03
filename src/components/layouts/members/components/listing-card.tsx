7d53181983edaec5b539676dd866641d1411b905
clsscrch <richardli121499@gmail.com>
2022-06-03 09:21:49 -0400

    Refactored listing card

--- src/components/layouts/members/components/listing-card.tsx
+++ src/components/layouts/members/components/listing-card.tsx
@@ -1,8 +1,8 @@
-import { Carousel, Image } from 'antd';
-import { LeftOutlined, RightOutlined } from '@ant-design/icons';
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 export const ListingCard: React.FC<any> = (props) => {
+
+  const params = useParams();
   // const imageGallery = props.images.map((image: any) => {
   //     return image;
   // })
@@ -16,24 +16,26 @@ export const ListingCard: React.FC<any> = (props) => {
       >
         <div className="relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0">
           <h2 className="mt-1 text-lg font-semibold text-white dark:sm:text-white">
-            ${props.data.listingDetail?.price}
+            ${props.data.price}
           </h2>
           {/* <h1 className="mt-1 text-lg font-semibold text-white dark:sm:text-white">Name</h1>
                     <p className="text-sm leading-4 font-medium text-white dark:sm:text-slate-400">Entire house</p> */}
         </div>
 
         <div className="grid gap-4 col-start-1 col-end-3 row-start-1">
-          {/* <img src={imageGallery[0]} className="w-full h-60 object-cover rounded-lg col-span-2 h-52"/>  */}
-          <img src="https://ssl.cdn-redfin.com/photo/235/islphoto/888/genIslnoResize.PAPH2122888_0.jpg"></img>
+          <img src={"https://ownercommunity.blob.core.windows.net/" + props.data.communityId + "/" + props.data.images[0]} className="w-full h-60 object-cover rounded-lg col-span-2 h-52" />
         </div>
 
         <div className="px-2">
-          <p className="mt-4 text-sm leading-6 col-start-1 dark:text-slate-400">
-            {props.data.listingDetail?.bedrooms} Bds, {props.data.listingDetail?.bathrooms} Ba,{' '}
-            {props.data.listingDetail?.squareFeet} sqft
+          <p className="mt-4 mb-0 text-sm leading-6 col-start-1 dark:text-slate-400">
+            {props.data.bedrooms} Bds, {props.data.bathrooms} Ba,{' '}
+            {props.data.squareFeet} sqft
           </p>
-          <p className="mt-4 text-sm leading-6 col-start-1 dark:text-slate-400">
+          <p className="mb-0 text-sm leading-6 col-start-1 dark:text-slate-400">
             <em> 123 Street St. City, AA 12345</em>
+          </p>
+          <p className="text-xs leading-6 col-start-1 dark:text-slate-400">
+            <em> A {props.data.type} in City</em>
           </p>
           <p className="mt-4 text-xs leading-6 col-start-1 dark:text-slate-400">Statefarm</p>
         </div>

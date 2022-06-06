7e8b4ed9ff168d181800845fa4dd2e28b374e1a8
clsscrch <richardli121499@gmail.com>
2022-06-06 12:08:43 -0400

    Added generateSasKey

--- infrastructure/services/maps/index.test.ts
+++ infrastructure/services/maps/index.test.ts
@@ -16,16 +16,32 @@ test('get map key', () => {
   
 });
 
-test('get account', () => {
-    
-    // arrange
+test('sas toke', async () => {
     const maps = new Maps();
-    
-    
-    // act
-    var account = maps.accountName; 
-    
-    // assert
-    expect(account).toBe('map name'); // expect test to fail - but helpul to see actual ouput
-    
+
+    var sasToken = await maps.generateSharedKey();
+
+    expect(sasToken).toBe("");
 });
+
+test("list token", async () => {
+    const maps = new Maps();
+
+    var token = await maps.listKeys();
+
+    expect(JSON.stringify(token)).toBe("");
+});
+
+// test('get account', () => {
+    
+//     // arrange
+//     const maps = new Maps();
+    
+    
+//     // act
+//     var account = maps.accountName; 
+    
+//     // assert
+//     expect(account).toBe('map name'); // expect test to fail - but helpul to see actual ouput
+    
+// });
--- infrastructure/services/maps/index.ts
+++ infrastructure/services/maps/index.ts
@@ -1,18 +1,29 @@
 import { AzureKeyCredential } from "@azure/search-documents";
-import { AzureMapsManagementClient } from "@azure/arm-maps";
+import { AccountsListKeysResponse, AzureMapsManagementClient, MapsAccount } from "@azure/arm-maps";
 import { AccountInfo } from "./account-info";
 import { throws } from "assert";
 import { AccountSasParameters } from "@azure/arm-maps";
+import { DefaultAzureCredential, DefaultAzureCredentialOptions, TokenCredential } from "@azure/identity";
+import dayjs from 'dayjs';
 
 
 export class Maps {
-    private readonly mapKeyEnvVar =  'MAPS_KEY';
+    private readonly mapKeyEnvVarKey =  'MAPS_KEY';
     private readonly _mapKey: string;
     private readonly _azureSubscriptionIDEnvVar: string =  'AZURE_SUBSCRIPTION_ID';
     private readonly _azureSubscriptionID: string;
     private readonly _azureMapsClient: AzureMapsManagementClient;
+    private readonly _resourceGroupKey: string = "RESOURCE_GROUP";
+    private readonly _resourceGroup: string;
+    private readonly _principalIdKey: string = "OBJECT_PRINCIPAL_ID";
+    private readonly _principalId: string;
+    private readonly _mapsAccountNameKey: "MAPS_ACCOUNT_NAME";
+    private readonly _mapsAccountName: string;
 
-    private readonly _accountName: string;
+    // private readonly _accountName: string;
+
+    private readonly _appIdentityKey: string = "AZURE_FUNCTION_APP_IDENTITY";
+    // private readonly _credentials: TokenCredential; 
 
     tryGetEnvVar(envVar: string): string {
         const value = process.env[envVar];
@@ -23,10 +34,15 @@ export class Maps {
     }
 
     constructor(){
-        const {accountName, accountKey} = AccountInfo.getInstance().getSettings();
-        this._mapKey = this.tryGetEnvVar(this.mapKeyEnvVar);
+        // const {accountName, accountKey} = AccountInfo.getInstance().getSettings();
+        this._mapKey = this.tryGetEnvVar(this.mapKeyEnvVarKey);
         this._azureSubscriptionID = this.tryGetEnvVar(this._azureSubscriptionIDEnvVar);
-        //this._azureMapsClient = new AzureMapsManagementClient(this.azureKeyCredential, this._azureSubscriptionID );
+        let credentials = new DefaultAzureCredential( { ManangedIdentityClientId: this.tryGetEnvVar(this._appIdentityKey) } as DefaultAzureCredentialOptions);
+        console.log(credentials);
+        this._azureMapsClient = new AzureMapsManagementClient(credentials, this._azureSubscriptionID );
+        this._resourceGroup = this.tryGetEnvVar(this._resourceGroupKey);
+        this._principalId = this.tryGetEnvVar(this._principalIdKey);
+        this._mapsAccountName = this.tryGetEnvVar("MAPS_ACCOUNT_NAME");
     }
 
     public get mapKey(): string {
@@ -39,12 +55,23 @@ export class Maps {
         return new AzureKeyCredential(this._mapKey);
     }
 
-    public get accountName(): string {
-        return this._accountName;
+    // public get accountName(): string {
+    //     return this._accountName;
+    // }
+
+    public async listKeys(): Promise<AccountsListKeysResponse> {
+        return await this._azureMapsClient.accounts.listKeys(this._resourceGroup, this._mapsAccountName);
     }
 
-    public generateSharedKey(resourceGroupName: string, accountName: string, mapsAccountSasParameters: AccountSasParameters): string {
-        //var mapRequest = (new )
-        return "";
+    // public listbyResourceGroup(): PagedAsyncIterableIterator<MapsAccount>
+
+    public async generateSharedKey(): Promise<string> {
+        try {
+            var sasToken = await this._azureMapsClient.accounts.listSas(this._resourceGroup, this._mapsAccountName, { expiry : dayjs().add(10, 'minutes').toISOString(), maxRatePerSecond: 10, principalId: this._principalId, start: dayjs().subtract(5, 'minutes').toISOString(), signingKey: this._mapKey} as AccountSasParameters); // ,  { expiry : dayjs().add(10, 'minutes').toISOString(), maxRatePerSecond: 10, principalId: "", start: dayjs().subtract(5, 'minutes').toISOString()} as AccountSasParameters
+            return sasToken.accountSasToken ?? "" ;
+        } catch (error) {
+            console.log(error);
+            return JSON.stringify(error);
+        }
     }
 }

d086b6d2a4eae9682129d57f12baece3cd255995
clsscrch <richardli121499@gmail.com>
2022-06-08 11:02:05 -0400

    Able to start inital search in property listing

--- src/components/layouts/members/components/properties-location.tsx
+++ src/components/layouts/members/components/properties-location.tsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AutoComplete } from 'antd';
+
+interface AddressDataType {
+  'value': string;
+}
 
 export const PropertiesLocation = (props: any) => {
-  console.log(props);
+  const [value, setValue] = useState('');
+  const [addresses, setAddresses] = useState<AddressDataType[]>([]) ;
+
+  const addressQuery = async (addressRequest: string) => {
+    var addresssGeocodeServiceUrlTemplate: string = 'https://atlas.microsoft.com/search/address/json?typeahead=true&api-version=1&query={query}';
+
+    var requestUrl = addresssGeocodeServiceUrlTemplate.replace('{query}', encodeURIComponent(addressRequest));
+    const token = props.data.property.mapSASToken;
+
+    const address = async () => { 
+
+      const request =  await fetch(requestUrl, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Authorization': 'jwt-sas ' + token,
+        'Content-Type': 'application/json; charset=utf-8'
+        }
+      });
+    
+      const data = await request.json();
+
+      return data.results;
+    }
+
+    return address();
+  }
+
+  const onChange = (data: any) => {
+    setValue(data);
+    let tmp: any[] = [];
+    addressQuery(data).then(data => {
+      data.forEach((address: any) => {
+        tmp.push({ "value" : address.address.freeformAddress})
+      });
+    }).finally(() => {
+      setAddresses(tmp);
+    });
+  }
+
   return (
     <div>
-      <h1>Properties Location goes here</h1>
-      <h2>{JSON.stringify(props)}</h2>
+      <p>Search:</p>
+      <AutoComplete
+        options={addresses}
+        style={{
+          width: 400,
+        }}
+        filterOption={(inputValue, value) =>
+          value?.value?.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+        }
+        value={value}
+        onChange={onChange}
+        
+      >
+      </AutoComplete>
+      
+      {/* <h2>{JSON.stringify(props)}</h2> */}
     </div>
   )
 }

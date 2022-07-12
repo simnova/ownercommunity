5202db856235ef5c87c73df8248b2b7365faba1a
clsscrch <richardli121499@gmail.com>
2022-07-12 11:01:43 -0400

    Service tickets now show on member site

--- graphql/data-sources/cosmos-db/service-tickets.ts
+++ graphql/data-sources/cosmos-db/service-tickets.ts
@@ -4,45 +4,43 @@ import { ServiceTicket, ServiceTicketModel } from '../../../infrastructure/data-
 import { Context } from '../../context';
 
 export class ServiceTickets extends MongoDataSource<ServiceTicket, Context> {
+  async getServiceTicketsByCommunityId(communityId: string): Promise<ServiceTicket[]> {
+    var dbData = await ServiceTicketModel.find({ community: communityId }).populate(['community', 'property', 'requestor', 'assignedTo']).exec();
 
-  async getServiceTicketsByCommunityId(communityId : string): Promise<ServiceTicket[]> {
-    var dbData = await ServiceTicketModel.find({community: communityId}).populate(['community','property','requestor','assignedTo']).exec();
-    
     //(await this.collection.find({community: communityId}))..  // await this.findByFields({community: communityId});
     return this.applyPermissionFilter(dbData, this.context);
   }
 
-  async getServiceTicketsOpenByRequestor(memberId : string): Promise<ServiceTicket[]> {
-    var dbData = await this.findByFields({requestor: memberId});
+  async getServiceTicketsOpenByRequestor(memberId: string): Promise<ServiceTicket[]> {
+    var dbData = await ServiceTicketModel.find({ requestor: memberId }).populate(['community', 'property', 'requestor', 'assignedTo']).exec();
     return this.applyPermissionFilter(dbData, this.context);
   }
 
-  async getServiceTicketsClosedByRequestor(memberId : string): Promise<ServiceTicket[]> {
-    var dbData = (await this.findByFields({requestor: memberId})).filter(ticket => ticket.status === 'CLOSED');
+  async getServiceTicketsClosedByRequestor(memberId: string): Promise<ServiceTicket[]> {
+    var dbData = (await this.findByFields({ requestor: memberId })).filter((ticket) => ticket.status === 'CLOSED');
     return this.applyPermissionFilter(dbData, this.context);
   }
-  async getServiceTicketsByAssignedTo(communityId : string, memberId:string): Promise<ServiceTicket[]> {
-    var dbData =  (await this.findByFields({community: communityId, assignedTo: memberId}))
+  async getServiceTicketsByAssignedTo(communityId: string, memberId: string): Promise<ServiceTicket[]> {
+    var dbData = await this.findByFields({ community: communityId, assignedTo: memberId });
     return this.applyPermissionFilter(dbData, this.context);
   }
 
-  
-
-  private async applyPermissionFilter(serviceTickets: ServiceTicket[], context: Context) : Promise<ServiceTicket[]>{
+  private async applyPermissionFilter(serviceTickets: ServiceTicket[], context: Context): Promise<ServiceTicket[]> {
     let converter = new ServiceTicketConverter();
-    
-    return (await Promise.all(serviceTickets.map(ticket => ticket))) //.populate(['community','property','requestor','assignedTo']))))
-      .map(ticket => converter.toDomain(ticket, context))
-      .filter( ticket => 
-          context.passport
-            .forServiceTicket(ticket)
-            .determineIf((permissions) => 
+
+    return (await Promise.all(serviceTickets.map((ticket) => ticket))) //.populate(['community','property','requestor','assignedTo']))))
+      .map((ticket) => converter.toDomain(ticket, context))
+      .filter((ticket) =>
+        context.passport
+          .forServiceTicket(ticket)
+          .determineIf(
+            (permissions) =>
               permissions.canManageTickets ||
               permissions.canAssignTickets ||
               (permissions.canCreateTickets && permissions.isEditingOwnTicket) ||
-              (permissions.canWorkOnTickets && permissions.isEditingAssignedTicket) 
-            ))
-      .map(ticket => converter.toMongo(ticket))
+              (permissions.canWorkOnTickets && permissions.isEditingAssignedTicket)
+          )
+      )
+      .map((ticket) => converter.toMongo(ticket));
   }
-
 }

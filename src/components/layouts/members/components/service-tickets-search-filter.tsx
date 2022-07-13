7a5205dc64c96aa82e7009ce21c28a958f248251
Duy Nguyen <nkduy2010@gmail.com>
2022-07-13 11:19:06 -0500

    fix autocomplete options

--- src/components/layouts/members/components/service-tickets-search-filter.tsx
+++ src/components/layouts/members/components/service-tickets-search-filter.tsx
@@ -1,6 +1,7 @@
 import { Collapse, Checkbox, Typography, AutoComplete } from 'antd';
 import { MinusOutlined, PlusOutlined, SearchOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
+import _ from 'lodash';
 const { Title } = Typography;
 const { Panel } = Collapse;
 const { Option } = AutoComplete;
@@ -12,59 +13,67 @@ interface ServiceTicketSearchFilterProps {
       name: string;
       count: number;
     }
-  ]
+  ];
   searchbar: boolean;
 }
 
-export const ServiceTicketsSearchFilter: React.FC<any> = (props: ServiceTicketSearchFilterProps) => {
-  const [options, setOptions] = useState<{value: string}[]>([]);
+export const ServiceTicketsSearchFilter: React.FC<any> = (
+  props: ServiceTicketSearchFilterProps
+) => {
+  const [options, setOptions] = useState<{ value: string }[]>([]);
 
   useEffect(() => {
     resetOptions();
   }, []);
 
-  const resetOptions = () => {
-    const uniqueNames: string[] = [];
-    props.options.forEach(element => {
-      const isDuplicate = uniqueNames.includes(element.name);
-      if (!isDuplicate) {
-        uniqueNames.push(element.name);
-        return true;
-      }
-      return false;
-    });
-    const uniqueOptions = uniqueNames.map(name => ({ value: name }));
-    console.log("UNIQUE OPTIONS ", uniqueOptions);
+  const getOptions = (value: string | undefined) => {
+    let uniqueNames: string[] = _.uniq(
+      props.options.map((option: any) => option.name)
+    );
+    if (value) {
+      uniqueNames = uniqueNames.filter((option) => {
+        return option.startsWith(value);
+      });
+    }
+    const uniqueOptions = uniqueNames.map((name) => ({ value: name }));
     setOptions(uniqueOptions);
-  }
+  };
+
+  const resetOptions = () => {
+    getOptions(undefined);
+  };
 
   const onChange = (value: string) => {
-    const newOptions = options.filter(option => {
-      return option.value.startsWith(value);
-    })
-    setOptions(newOptions);
-  }
+    getOptions(value);
+  };
 
   return (
     <>
       <Collapse
-        className="service-ticket-search-filter-collapse" 
-        defaultActiveKey={['1']} 
-        ghost 
+        className="service-ticket-search-filter-collapse"
+        defaultActiveKey={['1']}
+        ghost
         style={{ width: '80%' }}
-        expandIcon={({ isActive }) => isActive ? <MinusOutlined style={{ fontSize: '30px' }} /> : <PlusOutlined style={{ fontSize: '30px' }} />}
+        expandIcon={({ isActive }) =>
+          isActive ? (
+            <MinusOutlined style={{ fontSize: '30px' }} />
+          ) : (
+            <PlusOutlined style={{ fontSize: '30px' }} />
+          )
+        }
       >
-        <Panel 
+        <Panel
           header={
-            <Title 
-              level={5} 
-              style={{ fontWeight: 700, marginBottom: 0, alignSelf: 'center' }}>
-                {props.title}
+            <Title
+              level={5}
+              style={{ fontWeight: 700, marginBottom: 0, alignSelf: 'center' }}
+            >
+              {props.title}
             </Title>
-          } 
+          }
           key="1"
         >
-          {props.searchbar && 
+          {props.searchbar && (
             <AutoComplete
               options={options}
               placeholder="Search"
@@ -72,23 +81,29 @@ export const ServiceTicketsSearchFilter: React.FC<any> = (props: ServiceTicketSe
               onChange={onChange}
               onClear={resetOptions}
               allowClear
-          />}
-          {props.options && <div className='search-filter-scrollbar' style={{ maxHeight: '182px', overflowY: 'auto' }}>
-            {props.options.map((option: any) => {
-            return (
-              <div style={{ display: 'flex', justifyContent: 'space-between'}}>
-                <Checkbox>{option.name}</Checkbox>
-                <span>({option.count})</span>
-              </div>
-            )
-          })}
-          </div>}
-
+            />
+          )}
+          {props.options && (
+            <div
+              className="search-filter-scrollbar"
+              style={{ maxHeight: '182px', overflowY: 'auto' }}
+            >
+              {props.options.map((option: any) => {
+                return (
+                  <div
+                    style={{ display: 'flex', justifyContent: 'space-between' }}
+                  >
+                    <Checkbox>{option.name}</Checkbox>
+                    <span>({option.count})</span>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </Panel>
       </Collapse>
     </>
-  )
-}
+  );
+};
 
-function resetOptions(props: ServiceTicketSearchFilterProps) {
-}
+function resetOptions(props: ServiceTicketSearchFilterProps) {}

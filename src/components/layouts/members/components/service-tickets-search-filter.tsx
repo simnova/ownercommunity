9dbde61593a324594230985826d7d05e2516ac37
nnoce14 <nnoce12@gmail.com>
2022-07-13 11:02:20 -0400

    Worked on auto complete searchbar for search filter component

--- src/components/layouts/members/components/service-tickets-search-filter.tsx
+++ src/components/layouts/members/components/service-tickets-search-filter.tsx
@@ -1,8 +1,9 @@
-import { Collapse, Checkbox, Typography, Input } from 'antd';
+import { Collapse, Checkbox, Typography, AutoComplete } from 'antd';
 import { MinusOutlined, PlusOutlined, SearchOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
 const { Title } = Typography;
 const { Panel } = Collapse;
-const { Search } = Input;
+const { Option } = AutoComplete;
 
 interface ServiceTicketSearchFilterProps {
   title: string;
@@ -16,6 +17,34 @@ interface ServiceTicketSearchFilterProps {
 }
 
 export const ServiceTicketsSearchFilter: React.FC<any> = (props: ServiceTicketSearchFilterProps) => {
+  const [options, setOptions] = useState<{value: string}[]>([]);
+
+  useEffect(() => {
+    resetOptions();
+  }, []);
+
+  const resetOptions = () => {
+    const uniqueNames: string[] = [];
+    props.options.forEach(element => {
+      const isDuplicate = uniqueNames.includes(element.name);
+      if (!isDuplicate) {
+        uniqueNames.push(element.name);
+        return true;
+      }
+      return false;
+    });
+    const uniqueOptions = uniqueNames.map(name => ({ value: name }));
+    console.log("UNIQUE OPTIONS ", uniqueOptions);
+    setOptions(uniqueOptions);
+  }
+
+  const onChange = (value: string) => {
+    const newOptions = options.filter(option => {
+      return option.value.startsWith(value);
+    })
+    setOptions(newOptions);
+  }
+
   return (
     <>
       <Collapse
@@ -36,10 +65,13 @@ export const ServiceTicketsSearchFilter: React.FC<any> = (props: ServiceTicketSe
           key="1"
         >
           {props.searchbar && 
-            <Search 
-              allowClear 
-              placeholder='Search'
-              suffix={<SearchOutlined />}
+            <AutoComplete
+              options={options}
+              placeholder="Search"
+              style={{ width: '40%' }}
+              onChange={onChange}
+              onClear={resetOptions}
+              allowClear
           />}
           {props.options && <div className='search-filter-scrollbar' style={{ maxHeight: '182px', overflowY: 'auto' }}>
             {props.options.map((option: any) => {
@@ -56,4 +88,7 @@ export const ServiceTicketsSearchFilter: React.FC<any> = (props: ServiceTicketSe
       </Collapse>
     </>
   )
+}
+
+function resetOptions(props: ServiceTicketSearchFilterProps) {
 }

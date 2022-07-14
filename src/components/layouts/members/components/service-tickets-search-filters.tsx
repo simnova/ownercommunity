1099b37188a91912b93dc888f0a3fa9c8fceaf66
clsscrch <richardli121499@gmail.com>
2022-07-14 09:03:09 -0400

    Added priority

--- src/components/layouts/members/components/service-tickets-search-filters.tsx
+++ src/components/layouts/members/components/service-tickets-search-filters.tsx
@@ -22,6 +22,8 @@ interface FilterType {
 
 export const ServiceTicketsSearchFilters: React.FC<any> = (props) => {
   const [assignedToOptions, setAssignedToOptions] = useState<FilterType>();
+  const [priorityOptions, setPriorityOptions] = useState<FilterType>();
+
   const [filters, setFilters] = useState();
 
   useEffect(() => {
@@ -47,12 +49,51 @@ export const ServiceTicketsSearchFilters: React.FC<any> = (props) => {
     setAssignedToOptions(assignedTo);
   }, []);
 
+  useEffect(() => {
+    const priority: FilterType = {
+      title: 'Priority',
+      options: [],
+      id: 'priority'
+    };
+
+    const priorityValues = [1, 2, 3, 4, 5];
+
+    priorityValues.forEach((priorityValue) => {
+      const count =
+        props.searchData.facets.priority.find(
+          (t: any) => t.value === priorityValue.toString()
+        )?.count ?? 0;
+      priority.options.push({
+        name: priorityValue.toString(),
+        count: count ?? 0,
+        id: priorityValue.toString()
+      });
+    });
+    setPriorityOptions(priority);
+
+    // props.searchData.facets.priority.forEach(
+    //   (priorityFacet: {
+    //     value: string;
+    //     count: number;
+    //     __typename?: string;
+    //   }) => {
+    //     const count = priorityFacet.count;
+    //     priority.options.push({
+    //       name: priorityFacet.value,
+    //       count: count,
+    //       id: priorityFacet.value
+    //     });
+    //     setPriorityOptions(priority);
+    //   }
+    // );
+  }, []);
+
   // let filters: any = [assignedToOptions];
 
   console.log('props', props);
 
   const searchFilterConfig: SearchFilterConfigDefinition = {
-    filters: [assignedToOptions as FilterType]
+    filters: [assignedToOptions as FilterType, priorityOptions as FilterType]
   };
 
   return (

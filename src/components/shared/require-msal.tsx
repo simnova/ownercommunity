f6212ff8c2cbf16f3bfc7eb57defbd5bd99c3d4e
Eddie Tang <eddietang715@gmail.com>
2022-08-18 17:02:46 -0400

    updated msal to check for portal identifier

--- src/components/shared/require-msal.tsx
+++ src/components/shared/require-msal.tsx
@@ -1,52 +1,64 @@
 import RequireAuth from './require-auth';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { useMsal } from './msal-react-lite';
-
 export interface RequireMsalProps {
   identifier: string;
   forceLogin?: boolean;
 }
 
-const RequireMsal:React.FC<RequireMsalProps> = (params) => {
-  const { getIsLoggedIn, registerCallback, getSilentAuthResult, login } = useMsal();
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean|undefined>(getIsLoggedIn(params.identifier) === false ? false : undefined);
+const RequireMsal: React.FC<RequireMsalProps> = (params) => {
+  const { getIsLoggedIn, registerCallback, getSilentAuthResult, login } =
+    useMsal();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | undefined>(
+    getIsLoggedIn(params.identifier) === false ? false : undefined
+  );
 
   useEffect(() => {
     const determineIfUserHasActiveSession = async () => {
-      const authResult = getSilentAuthResult(params.identifier);
-      if(authResult !== undefined) {
+      const authResult = await getSilentAuthResult(params.identifier);
+      if (authResult !== undefined) {
+       if((authResult.account?.idTokenClaims as any).externalAppKey === process.env.REACT_APP_PORTAL_IDENTIFIER) {
         setIsAuthenticated(true);
-      } 
+       }
+        setIsAuthenticated(false);
+      }
+    };
+    console.log('lala', isAuthenticated);
+    if (isAuthenticated === undefined) {
+      //we are not certain the user has an active session
+      console.log('lala');
+      determineIfUserHasActiveSession().catch((e) => console.error(e));
     }
-    console.log('lala',isAuthenticated)
-    if(isAuthenticated === undefined){ //we are not certain the user has an active session
-      console.log('lala')
-      determineIfUserHasActiveSession().catch(e => console.error(e));
-    }
-    if(params.forceLogin && params.forceLogin === true && isAuthenticated === false){
+    if (
+      params.forceLogin &&
+      params.forceLogin === true &&
+      isAuthenticated === false
+    ) {
       login(params.identifier);
     }
-  }, [isAuthenticated])
-
+  }, [isAuthenticated]);
 
   useEffect(() => {
-    const processLoginResult = async (result:boolean): Promise<void> => {
-      console.log('processLoginResult',result);
-      if(params.forceLogin && params.forceLogin === true && result === false){
-        await login(params.identifier)
-      }else{
+    const processLoginResult = async (result: boolean): Promise<void> => {
+      console.log('processLoginResult', result);
+      if (params.forceLogin && params.forceLogin === true && result === false) {
+        await login(params.identifier);
+      } else {
         setIsAuthenticated(result);
       }
-    }
-    registerCallback(params.identifier,(isLoggedIn) => 
-      processLoginResult(isLoggedIn).catch(e => console.error(e))
+    };
+    registerCallback(params.identifier, (isLoggedIn) =>
+      processLoginResult(isLoggedIn).catch((e) => console.error(e))
     );
-  }, [params,registerCallback,login,setIsAuthenticated]);
-  
-  return (params.forceLogin && params.forceLogin === true && (typeof isAuthenticated === 'undefined' || isAuthenticated === false)) ?  null : 
-  (
+  }, [params, registerCallback, login, setIsAuthenticated]);
 
-    <RequireAuth isAuthenticated={isAuthenticated}>{params.children}</RequireAuth>
-  )
-}
+  return params.forceLogin &&
+    params.forceLogin === true &&
+    (typeof isAuthenticated === 'undefined' ||
+      isAuthenticated === false) ? null : (
+    <RequireAuth isAuthenticated={isAuthenticated}>
+      {params.children}
+    </RequireAuth>
+  );
+};
 export default RequireMsal;

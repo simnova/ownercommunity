aef465c878b4840a1dfa89ef0b9885c369297f3b
zmadvero <madscientist2854@gmail.com>
2023-05-17 11:28:03 -0400

    use preloaded relay queries

--- src/components/layouts/accounts/components/community-list.container.tsx
+++ src/components/layouts/accounts/components/community-list.container.tsx
@@ -2,8 +2,9 @@
 import { useQuery } from '@apollo/client';
 import { CommunityList } from './community-list';
 import { CommunityListContainerCommunitiesQueryDocument } from '../../../../generated';
-import graphql from 'babel-plugin-relay/macro';
-import { useLazyLoadQuery } from 'react-relay';
+import { graphql } from 'babel-plugin-relay/macro';
+import { loadQuery, useLazyLoadQuery, usePreloadedQuery, useRelayEnvironment } from 'react-relay';
+import { communityListContainerCommunitiesQuery } from './__generated__/communityListContainerCommunitiesQuery.graphql';
 
 // type CommunityFragment = FragmentType<typeof CommunityListContainerCommunitiesFieldsFragmentDoc>
 const q = graphql`
@@ -15,9 +16,10 @@ const q = graphql`
 `
 
 export const CommunityListContainer: React.FC<any> = (props) => {
+  const qref = loadQuery<communityListContainerCommunitiesQuery>( useRelayEnvironment(), q, {} )
 
   // const { loading, error, data} = useQuery(CommunityListContainerCommunitiesQueryDocument);
-  const data =  useLazyLoadQuery(q, {})
+  const data =  usePreloadedQuery<communityListContainerCommunitiesQuery>(q, qref)
   console.log(data)
   const error = false
   const loading = false

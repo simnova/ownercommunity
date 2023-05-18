a1aaae81eef1a8688af940fd9c9494b1b304119f
Nick Noce <nnoce12@gmail.com>
2023-05-18 15:54:07 -0400

    Added DataSourceBuilder class to give dataSources field correct typing on Context

--- graphql/context.ts
+++ graphql/context.ts
@@ -4,12 +4,7 @@ import { HttpRequest } from '@azure/functions';
 import { ApolloServerRequestHandler } from './init/apollo';
 import { decorateContext } from './init/extensions/passport-context';
 import { Services } from '../infrastructure/services';
-import { CommunityBlobAPI, MemberBlobAPI, PropertyBlobAPI } from './data-sources/blob';
-import { PropertySearchAPI, ServiceTicketsSearchAPI } from './data-sources/cognitive-search';
-import { CommunityCosmosdbAPI, CommunityModel, MemberCosmosdbAPI, MemberModel, PropertyCosmosdbAPI, PropertyModel, RoleCosmosdbAPI, RoleModel, ServiceCosmosdbAPI, ServiceModel, ServiceTicketCosmosdbAPI, ServiceTicketModel, UserCosmosdbAPI, UserModel } from './data-sources/cosmos-db';
-import { CommunityDomainAPI, CommunityUnitOfWork, MemberDomainAPI, MemberUnitOfWork, PropertyDomainAPI, PropertyUnitOfWork, RoleDomainAPI, RoleUnitOfWork, ServiceDomainAPI, ServiceTicketDomainAPI, ServiceTicketUnitOfWork, ServiceUnitOfWork, UserDomainAPI, UserUnitOfWork } from './data-sources/domain';
-import { PropertyMapAPI } from './data-sources/maps';
-import { CommunityVercelAPI } from './data-sources/vercel';
+import { DataSourceBuilder } from './data-sources/data-source-builder';
 
 export class Context {
   public verifiedUser: {
@@ -18,7 +13,7 @@ export class Context {
   };
   public community: string;
   public passport: Passport;
-  public dataSources: any;
+  public dataSources: DataSourceBuilder;
   public executionContext: any;
   public services: Services;
 
@@ -28,29 +23,7 @@ export class Context {
 
     this.services = new Services();
 
-    this.dataSources = {
-      communityBlobAPI: new CommunityBlobAPI({ context: this }),
-      memberBlobAPI: new MemberBlobAPI({ context: this }),
-      propertyBlobAPI: new PropertyBlobAPI({ context: this }),
-      propertySearchApi: new PropertySearchAPI({ context: this }),
-      serviceTicketsSearchApi: new ServiceTicketsSearchAPI({ context: this }),
-      userCosmosdbApi: new UserCosmosdbAPI({ collection: UserModel, context: this }),
-      roleCosmosdbApi: new RoleCosmosdbAPI({ collection: RoleModel, context: this }),
-      serviceCosmosdbApi: new ServiceCosmosdbAPI({ collection: ServiceModel, context: this }),
-      serviceTicketCosmosdbApi: new ServiceTicketCosmosdbAPI({ collection: ServiceTicketModel, context: this }),
-      memberCosmosdbApi: new MemberCosmosdbAPI({ collection: MemberModel, context: this }),
-      communityCosmosdbApi: new CommunityCosmosdbAPI({ collection: CommunityModel, context: this }),
-      propertyCosmosdbApi: new PropertyCosmosdbAPI({ collection: PropertyModel, context: this }),
-      userDomainAPI: new UserDomainAPI({ unitOfWork: UserUnitOfWork, context: this }),
-      communityDomainAPI: new CommunityDomainAPI({ unitOfWork: CommunityUnitOfWork, context: this }),
-      memberDomainAPI: new MemberDomainAPI({ unitOfWork: MemberUnitOfWork, context: this }),
-      roleDomainAPI: new RoleDomainAPI({ unitOfWork: RoleUnitOfWork, context: this }),
-      propertyDomainAPI: new PropertyDomainAPI({ unitOfWork: PropertyUnitOfWork, context: this }),
-      serviceDomainAPI: new ServiceDomainAPI({ unitOfWork: ServiceUnitOfWork, context: this }),
-      serviceTicketDomainAPI: new ServiceTicketDomainAPI({ unitOfWork: ServiceTicketUnitOfWork, context: this }),
-      propertyMapApi: new PropertyMapAPI({ context: this }),
-      communityVercelApi: new CommunityVercelAPI({ context: this }),
-    };
+    this.dataSources = new DataSourceBuilder(this);
 
     if (bearerToken) {
       let verifiedUser = await serverRequestHandler.getPortalTokenExtractor().GetVerifiedUser(bearerToken);

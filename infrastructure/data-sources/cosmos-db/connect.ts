fead514f9fd0748dd597003119b44f9d5637a891
Nick Noce <nnoce12@gmail.com>
2023-05-24 08:10:58 -0400

    Removed console log which was outputting cosmosdb connection string

--- infrastructure/data-sources/cosmos-db/connect.ts
+++ infrastructure/data-sources/cosmos-db/connect.ts
@@ -62,8 +62,7 @@ export const connect = async () => {
     connectionString =`${process.env.COSMOSDB}/?ssl=true&replicaSet=globaldb&retrywrites=false&appName=@sharethrift@`;
     console.log("Connecting to CosmosDB in Dev / Prod Mode");
   }
-  
-  console.log(`Connecting to CosmosDB: ${connectionString}`);
+
   try {
     await mongoose.connect(connectionString, {
       //  useNewUrlParser: true, 

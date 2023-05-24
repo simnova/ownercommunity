903d21f81592443652db9ac52cb453f094c78ce1
Nick Noce <nnoce12@gmail.com>
2023-05-24 11:49:00 -0400

    Changed onClick

--- src/components/layouts/shared/components/properties-listing-image-list.tsx
+++ src/components/layouts/shared/components/properties-listing-image-list.tsx
@@ -1,29 +1,39 @@
-import { Col, Row, Image } from "antd";
-import { useParams } from "react-router-dom";
+import { Col, Row, Image } from 'antd';
+import { useParams } from 'react-router-dom';
 
 interface PropertiesListingImageListProps {
   data: {
-    images?: (string|null)[];
-  }
+    images?: (string | null)[];
+  };
 }
 
 export const PropertiesListingImageList: React.FC<PropertiesListingImageListProps> = (props) => {
   const params = useParams();
-  
+
   return (
     <>
       {props.data.images && (
         <Row>
           <Col span={24}>
-            {props.data.images.map((image, index) => {   
-              if (image !== null) { 
+            {props.data.images.map((image, index) => {
+              if (image !== null) {
                 const url = `https://ownercommunity.blob.core.windows.net/${params.communityId}/${image}`;
-                return <Image className="property-listing-image" style={{ padding: "0px 10px", width: "200px" }} src={url} key={index} alt={`listing-image${index+1}`} preview={false} onClick={() => console.log("Hello world")} />
+                return (
+                  <Image
+                    className="property-listing-image"
+                    style={{ padding: '0px 10px', width: '200px' }}
+                    src={url}
+                    key={index}
+                    alt={`listing-image${index + 1}`}
+                    preview={false}
+                    onClick={() => console.log('Delete me')}
+                  />
+                );
               }
             })}
           </Col>
         </Row>
       )}
     </>
-  )
-}
+  );
+};

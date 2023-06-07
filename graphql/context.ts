5edaf5ead38496b2d833d960d0b7f5688607c5b6
Nick Noce <nnoce12@gmail.com>
2023-06-07 13:59:00 -0400

    Refactored passport context into a class

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,5 +1,4 @@
 import { Passport } from '../domain/contexts/iam/passport';
-import * as util from './init/extensions/util';
 import { HttpRequest } from '@azure/functions';
 import { ApolloServerRequestHandler } from './init/apollo';
 import { PassportContext } from './init/extensions/passport-context';
@@ -18,25 +17,10 @@ export class Context {
   public services: Services;
 
   public async init(req: HttpRequest, serverRequestHandler: ApolloServerRequestHandler) {
-    let bearerToken = util.ExtractBearerToken(req);
-    console.log('[BearerToken]: ', bearerToken);
-
     this.services = new Services();
-
     this.dataSources = new DataSourceBuilder(this);
 
-    // if (bearerToken) {
-    //   let verifiedUser = await serverRequestHandler.getPortalTokenExtractor().GetVerifiedUser(bearerToken);
-    //   console.log('Decorating context with verified user:', JSON.stringify(verifiedUser));
-    //   if (verifiedUser) {
-    //     this.verifiedUser = verifiedUser;
-    //     console.log('context value is now:', this);
-    //   }
-    // }
-
-    await PassportContext.decorateContext(this);
-
-    // await decorateContext(this, req);
+    await PassportContext.decorateContext(this, req, serverRequestHandler.getPortalTokenExtractor());
 
     req.headers['x-ms-privatelink-id'] = ''; // https://github.com/Azure/azure-functions-host/issues/6013
     req.headers['server'] = null; //hide microsoft server header
--- graphql/init/extensions/passport-context.ts
+++ graphql/init/extensions/passport-context.ts
@@ -1,4 +1,4 @@
-import { PassportImpl, Passport, ReadOnlyPassport, SystemPassport } from "../../../domain/contexts/iam/passport";
+import { PassportImpl, ReadOnlyPassport } from "../../../domain/contexts/iam/passport";
 import { Context } from "../../context";
 
 import { UserConverter } from '../../../domain/infrastructure/persistence/user.domain-adapter';
@@ -16,15 +16,25 @@ export class PassportContext {
   context: Partial<Context>;
 
   constructor(
-    private env: string,
     private req: HttpRequest,
     context: Partial<Context>,
     private portalTokenValidator: PortalTokenValidation,
   ) { this.context = context; }
 
+  static async decorateContext(context: Partial<Context>, req: HttpRequest, portalTokenValidator: PortalTokenValidation): Promise<void> {
+    const passportContext = new PassportContext(
+      req,
+      context,
+      portalTokenValidator
+    );
+    await passportContext.setContextVerifiedJwt();
+    await passportContext.setContextPassport();
+  }
+
   private async setContextVerifiedJwt(): Promise<void> {
     let bearerToken = util.ExtractBearerToken(this.req);
     if (bearerToken) {
+      console.log('[BearerToken] ', bearerToken);
       let verifiedUser = await this.portalTokenValidator.GetVerifiedUser(bearerToken);
       console.log('Decorating context with verified user:', JSON.stringify(verifiedUser));
       if (verifiedUser) {
@@ -36,82 +46,19 @@ export class PassportContext {
   private async setContextPassport(): Promise<void> {
     let communityHeader = this.req.headers['community'];
     if (communityHeader) {
-      let mongoCommunity = await getCommunityByHeader(communityHeader);
+      let mongoCommunity = await this.getCommunityByHeader(communityHeader);
       if (this.context.verifiedUser && this.context.verifiedUser.verifiedJWT && this.context.verifiedUser.verifiedJWT.sub && mongoCommunity) {
-        this.context.passport = await getPassport(this.context, mongoCommunity);
+        this.context.passport = await this.getPassport(this.context, mongoCommunity);
         this.context.community = mongoCommunity.id;
         console.log(' == CONTEXT DECORATED SUCCESSFULLY == ');
+        return;
+      }
     }
-
-
+    this.context.passport = ReadOnlyPassport.GetInstance();
+    this.context.community = null;
   }
 
-  static async decorateContext(context: Partial<Context>, env: string, req: HttpRequest, portalTokenValidator: PortalTokenValidation): Promise<void> {
-    console.log('context', context);
-    const passportContext = new PassportContext(
-      env,
-      req,
-      context,
-      portalTokenValidator
-    );
-    await passportContext.setContextVerifiedJwt();
-  }
-
-}
-
-export const decorateContext = async (context: Partial<Context>, req:HttpRequest): Promise<void> => {
-  let communityHeader = req.headers['community'];
-  if(!communityHeader || communityHeader === '') {
-    console.log(' == ERROR 0== ', context);
-    context.passport = ReadOnlyPassport.GetInstance();
-    context.community = null;
-  }
-  let mongoCommunity = await getCommunityByHeader(communityHeader);
-  if(
-    !context.verifiedUser || 
-    !context.verifiedUser.verifiedJWT ||
-    !context.verifiedUser.verifiedJWT.sub ||
-    !mongoCommunity
-    ) {
-      console.log(' == ERROR 1== ', mongoCommunity, context);
-      context.passport = ReadOnlyPassport.GetInstance();
-      context.community = null;
-  }
-  try {
-    context.passport = await getPassport(context, mongoCommunity);
-    context.community = mongoCommunity.id;   
-    console.log(' == CONTEXT DECORATED SUCCESSFULLY == ');
-  } catch (error) {
-    console.log(' == ERROR 2== ', error);
-    context.passport = ReadOnlyPassport.GetInstance();
-    context.community = null;
-  }
-}
-
-const getPassport = async (context: Partial<Context>, mongoCommunity: Community) => {
-  let readOnlyPassport = ReadOnlyPassport.GetInstance();
-  let userExternalId = context.verifiedUser.verifiedJWT.sub;
-  let mongoUser = await UserModel.findOne({externalId:userExternalId}).exec();
-  let mongoMember = (
-      await (MemberModel
-        .findOne({community: mongoCommunity.id, 'accounts.user': mongoUser.id})
-        .populate('community')
-        .populate('accounts.user')
-        .populate('role')
-        .exec())
-      )
-
-  if(mongoCommunity && mongoMember && mongoUser) {
-    let userDo = new UserConverter().toDomain(mongoUser, {passport: readOnlyPassport});
-    let memberDo = new MemberConverter().toDomain(mongoMember,{passport: readOnlyPassport});
-    let communityDo = new CommunityConverter().toDomain(mongoCommunity, {passport: readOnlyPassport});
-    return new PassportImpl(userDo, memberDo, communityDo);
-  }
-  console.log(' == ERROR == ');
-  return readOnlyPassport;
-}
-
-const getCommunityByHeader = async ( header: string): Promise<Community>  => {
+  private async getCommunityByHeader( header: string): Promise<Community>  {
     if(isValidObjectId(header)) {
       console.log('valid header!objectId');
       return CommunityModel.findById(header);
@@ -124,4 +71,57 @@ const getCommunityByHeader = async ( header: string): Promise<Community>  => {
       ]
       });
   }
-  {}
+
+  private async getPassport(context: Partial<Context>, mongoCommunity: Community) {
+    let readOnlyPassport = ReadOnlyPassport.GetInstance();
+    let userExternalId = context.verifiedUser.verifiedJWT.sub;
+    let mongoUser = await UserModel.findOne({externalId:userExternalId}).exec();
+    let mongoMember = (
+        await (MemberModel
+          .findOne({community: mongoCommunity.id, 'accounts.user': mongoUser.id})
+          .populate('community')
+          .populate('accounts.user')
+          .populate('role')
+          .exec())
+        )
+  
+    if(mongoCommunity && mongoMember && mongoUser) {
+      let userDo = new UserConverter().toDomain(mongoUser, {passport: readOnlyPassport});
+      let memberDo = new MemberConverter().toDomain(mongoMember,{passport: readOnlyPassport});
+      let communityDo = new CommunityConverter().toDomain(mongoCommunity, {passport: readOnlyPassport});
+      return new PassportImpl(userDo, memberDo, communityDo);
+    }
+    console.log(' == ERROR == ');
+    console.log('verifiedUser: ', context.verifiedUser, ' community: ', mongoCommunity);
+    return readOnlyPassport;
+  }
+}
+
+// export const decorateContext = async (context: Partial<Context>, req:HttpRequest): Promise<void> => {
+//   let communityHeader = req.headers['community'];
+//   if(!communityHeader || communityHeader === '') {
+//     console.log(' == ERROR 0== ', context);
+//     context.passport = ReadOnlyPassport.GetInstance();
+//     context.community = null;
+//   }
+//   let mongoCommunity = await getCommunityByHeader(communityHeader);
+//   if(
+//     !context.verifiedUser || 
+//     !context.verifiedUser.verifiedJWT ||
+//     !context.verifiedUser.verifiedJWT.sub ||
+//     !mongoCommunity
+//     ) {
+//       console.log(' == ERROR 1== ', mongoCommunity, context);
+//       context.passport = ReadOnlyPassport.GetInstance();
+//       context.community = null;
+//   }
+//   try {
+//     context.passport = await getPassport(context, mongoCommunity);
+//     context.community = mongoCommunity.id;   
+//     console.log(' == CONTEXT DECORATED SUCCESSFULLY == ');
+//   } catch (error) {
+//     console.log(' == ERROR 2== ', error);
+//     context.passport = ReadOnlyPassport.GetInstance();
+//     context.community = null;
+//   }
+// }

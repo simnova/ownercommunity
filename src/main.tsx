213edc5b1be49dab097e549da07c386a92381670
zmadvero <za366@drexel.edu>
2023-06-21 14:44:07 -0400

    change index.tsx to main.tsx for vite

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,8 @@ import featureFlagConfig from './config/feature-flag-config';
 import MaintenanceMessageProvider from './components/shared/maintenance-message';
 import { CachePurgeProvider } from './contexts/CachePurgeContext';
 
+console.log("env", import.meta.env)
+
 function ConfigProviderWrapper() {
   
 

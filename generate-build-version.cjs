302b1c6c6b0d4ac5b96973bd0e91962647089988
Reggie Easton <reggie.easton5@gmail.com>
2023-12-20 09:50:27 -0500

    Update build version and package.json script

--- generate-build-version.cjs
+++ generate-build-version.cjs
@@ -1,28 +1,28 @@
-import fs from "fs";
-import path from "path";
+const fs = require('fs');
+const path = require('path');
 
 // Read the current version from meta.json
-const metaPath = path.join(__dirname, "public", "meta.json");
+const metaPath = path.join(__dirname, 'public', 'meta.json');
 const metaData = require(metaPath);
 const currentVersion = metaData.version;
 
 // Determine the desired version update type (default: patch)
-const updateType = process.argv[2] || "patch";
-const versionParts = currentVersion.split(".");
+const updateType = process.argv[2] || 'patch';
+const versionParts = currentVersion.split('.');
 let [major, minor, patch] = versionParts.map(Number);
 
 // Update the version based on the specified update type
 switch (updateType) {
-  case "major":
+  case 'major':
     major++;
     minor = 0;
     patch = 0;
     break;
-  case "minor":
+  case 'minor':
     minor++;
     patch = 0;
     break;
-  case "patch":
+  case 'patch':
   default:
     patch++;
 }

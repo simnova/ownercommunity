bbe3a282f6e25b02d4fdfaaf0817ea202ee5630c
Duy Nguyen <nkduy2010@gmail.com>
2024-01-24 21:33:48 -0500

    fix loading wrong community in site editor+add button to add new page for new community

--- src/components/shared/blob-to-local-storage.tsx
+++ src/components/shared/blob-to-local-storage.tsx
@@ -1,78 +1,71 @@
-import { useEffect, useState } from "react";
-import { usePageLayouts } from "../editor/local-data";
-
+import { useEffect, useState } from 'react';
+import { usePageLayouts } from '../editor/local-data';
 
 export interface BlobToLocalStorageProps {
-
-    communityId?: string ;
-  
+  communityId?: string;
 }
- 
 
 export const BlobToLocalStorage: React.FC<BlobToLocalStorageProps> = (props) => {
   const [loading, setLoading] = useState(true);
   const [, setPageLayouts] = usePageLayouts();
 
-
   useEffect(() => {
-
-    const tryGetPageLayouts = async (communityId:string):Promise<[any]|undefined> => {
-        try {
-        const api_call = await fetch(`https://ownercommunity.blob.core.windows.net/${ communityId }/website-root`); 
-        return api_call.json();
-      } catch (error) {
-        console.log('app: cannot find pageLayouts from URL:',error);
-      }
-    }
-
-    const tryGetCommunityId = async() : Promise<string|undefined> => {
+    console.log('BlobToLocalStorage useEffect');
+    const tryGetPageLayouts = async (communityId: string): Promise<[any] | undefined> => {
       try {
-        // temp fix for owner.community blob
-        // let hostName = window.location.hostname;
-        // if (hostName === "owner.community" || hostName === "ownercommunity-ui.pages.dev") {
-        //   hostName = "owners.atlantisocmd.com"
-        // }
+        const api_call = await fetch(`https://ownercommunity.blob.core.windows.net/${communityId}/website-root`);
+        if (!api_call.ok) {
+          console.log('app: cannot find pageLayouts from URL - Status:', api_call.status);
+          return undefined; // or handle the error as needed
+        }
 
-        // temp fix for the above temp fix - there are more urls we have to rewrite than just owner.community
-        const hostName = 'owners.atlantisocmd.com';
-        const special_ports = ['80', '3000']; // Not sure why we append ports to the blob storage, but adding this just in case it's needed
+        return api_call.json();
 
+      } catch (error) {
+        console.log('app: cannot find pageLayouts from URL:', error);
+      }
+    };
+
+    const tryGetCommunityId = async (): Promise<string | undefined> => {
+      try {
+        console.log('tryGetCommunityId');
         const api_call = await fetch(
           `https://ownercommunity.blob.core.windows.net/community-domains/${
-            hostName +
-            (window.location.port && (!special_ports.includes(window.location.port) ? ':' + window.location.port : ''))
+            window.location.hostname +
+            (window.location.port && window.location.port !== '80' ? ':' + window.location.port : '')
           }`
         );
-
-        console.log('Blob Storage fetch call: ', api_call)
-
         const data = await api_call.json();
-        if(data && data.communityId ){
-          console.log('community-id:',data.communityId);
+        if (data?.communityId) {
+          console.log('community-id:', data.communityId);
           return data.communityId;
         }
       } catch (error) {
-        console.log('app: cannot find community from URL:',error);
+        console.log('app: cannot find community from URL:', error);
       }
-    }
+    };
 
-    const loadLocalStorage = async (communityId:string|undefined) : Promise<void> => {
-      if(typeof communityId === 'undefined' || communityId === ''){
+    const loadLocalStorage = async (communityId: string | undefined): Promise<void> => {
+      if (typeof communityId === 'undefined' || communityId === '') {
         communityId = await tryGetCommunityId();
       }
-      if(typeof communityId !== 'undefined' && communityId !== ''){
-        localStorage.setItem('community',communityId);
+      if (typeof communityId !== 'undefined' && communityId !== '') {
+        localStorage.setItem('community', communityId);
         const pageLayoutsBlobValue = await tryGetPageLayouts(communityId);
-        if(typeof pageLayoutsBlobValue !== 'undefined'){
+        if (typeof pageLayoutsBlobValue !== 'undefined') {
           setPageLayouts(pageLayoutsBlobValue);
+          localStorage.setItem('pageLayouts', JSON.stringify(pageLayoutsBlobValue));
+        } else {
+          localStorage.setItem('pageLayouts', JSON.stringify(''));
         }
       }
-    }
+    };
 
-    loadLocalStorage(props.communityId).catch(error => console.log('app: cannot find community to load into local storage:',error));
+    loadLocalStorage(props.communityId).catch((error) =>
+      console.log('app: cannot find community to load into local storage:', error)
+    );
     setLoading(false);
-    
-  }, [setLoading,props.communityId]);
+  }, [setLoading, props.communityId]);
 
-  return loading ? <></>:<>{props.children}</>;
-}
+  return loading ? <></> : <>{props.children}</>;
+};

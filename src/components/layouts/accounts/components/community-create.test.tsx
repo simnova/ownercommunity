045c0c726da7e548ca236f885ae163001e42a248
Duy Nguyen <nkduy2010@gmail.com>
2024-01-31 13:46:43 -0500

    add tests on input for community create

---
+++ src/components/layouts/accounts/components/community-create.test.tsx
@@ -0,0 +1,48 @@
+import { userEvent } from '@storybook/test';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { CommunityCreate } from './community-create';
+import { act } from 'react-dom/test-utils';
+
+describe('initially,', () => {
+  it('should have Name input empty', () => {
+    render(<CommunityCreate />);
+    const element = screen.getByLabelText('Name') as HTMLInputElement;
+    expect(element.value).toBe('');
+  });
+});
+
+describe('initially, when the Name input is empty, click Create Community', () => {
+  it('should have error Please input Name!', async () => {
+    render(<CommunityCreate />);
+    // const user = userEvent.setup();
+    const errorElement = screen.queryByText(/please input name!/i) as HTMLElement;
+    expect(errorElement).not.toBeInTheDocument();
+    const createButton = screen.getByText('Create Community');
+    await userEvent.click(createButton);
+    await waitFor(() => {
+      const errorElement = screen.queryByText(/please input name!/i) as HTMLElement;
+      expect(errorElement).toBeInTheDocument();
+    });
+  });
+});
+
+describe('when input Name, then clear the input', () => {
+  it('should have error Please input Name!', async () => {
+    render(<CommunityCreate />);
+    // const user = userEvent.setup();
+    const nameInput = screen.getByLabelText('Name') as HTMLInputElement;
+
+    // user interacts with the input
+    await act(async () => {
+      await userEvent.type(nameInput, 'test');
+      await userEvent.clear(nameInput);
+      
+    });
+
+    // need to put in await waitFor() to wait for the error to appear
+    await waitFor(() => {
+      const errorElement = screen.queryByText(/please input name!/i) as HTMLElement;
+      expect(errorElement).toBeInTheDocument();
+    });
+  });
+});

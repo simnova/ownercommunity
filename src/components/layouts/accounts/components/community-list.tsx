ffe96d5f0e790ede1ec517f613d8f45de0b178da
Duy Nguyen <nkduy2010@gmail.com>
2024-02-05 16:26:09 -0500

    add type def to props of community-list

--- src/components/layouts/accounts/components/community-list.tsx
+++ src/components/layouts/accounts/components/community-list.tsx
@@ -1,28 +1,27 @@
 import { Button, Col, Row } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { LocalSettingsKeys } from '../../../../constants';
+import { Community } from '../../../../generated';
 
 export interface CommunityListProps {
   data: {
-    communities: {
-      id: string;
-      name: string;
-    }[];
+    communities: Community[];
   };
 }
 
-export const CommunityList: React.FC<any> = (props) => {
+export const CommunityList: React.FC<CommunityListProps> = (props) => {
   const navigate = useNavigate();
   
   return (
     <div>
       <h1>Navigate to a Community</h1>
 
-      {props.data.communities.map((community: any) => (
+      {props.data?.communities?.map((community: any) => (
         <div key={community.id}>
           <Row justify="center">
             <Col span={8} style={{ textAlign: 'center', borderRight: 'solid 1px' }}>
               <Button
+                data-testid="community-list-button"
                 style={{ width: '250px', marginBottom: '10px' }}
                 onClick={() =>
                   navigate(
@@ -38,6 +37,7 @@ export const CommunityList: React.FC<any> = (props) => {
             { community.userIsAdmin && (
             <Col span={8} style={{ textAlign: 'center' }}>
               <Button
+                data-testid="community-list-admin-button"
                 style={{ width: '250px' }}
                 onClick={() => navigate(`/community/${community.id}/admin`)}
               >

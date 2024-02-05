95cb55cb8eaf5ed399ad77744c7b01167f2d88d0
Duy Nguyen <nkduy2010@gmail.com>
2024-02-05 13:26:59 -0500

    add navigation test

--- src/components/layouts/accounts/accounts.test.tsx
+++ src/components/layouts/accounts/accounts.test.tsx
@@ -1,10 +1,11 @@
+import * as ApolloClient from '@apollo/client';
+import { render, screen, waitFor } from '@testing-library/react';
+import * as Auth from 'react-oidc-context';
 import { AuthProvider } from 'react-oidc-context';
 import { MemoryRouter } from 'react-router-dom';
-import { oidcConfig } from '../../../config/odic-config';
-import { render, waitFor, screen } from '@testing-library/react';
 import App from '../../../App';
-import * as Auth from 'react-oidc-context';
-import * as ApolloClient from '@apollo/client';
+import { oidcConfig } from '../../../config/odic-config';
+import { act } from 'react-dom/test-utils';
 
 // unauthorized user
 describe('given not authorized, when navigating to community/accounts', () => {
@@ -27,6 +28,49 @@ describe('given not authorized, when navigating to community/accounts', () => {
 // authorized user
 describe('given authorized user named Duy Nguyen, when navigating to community/accounts', () => {
   it('should display /community/accounts screen, and display Duy Nguyen', async () => {
+    // set up authenticated environment
+    const useAuthSpy = vi.spyOn(Auth, 'useAuth');
+    const mockResolveValue = {
+      isAuthenticated: true
+    };
+    useAuthSpy.mockReturnValue(mockResolveValue as any);
+
+    // set up user data from useQuery
+    const useQuerySpy = vi.spyOn(ApolloClient, 'useQuery');
+    const mockQueryValue = {
+      loading: false,
+      error: undefined,
+      data: {
+        userCurrent: {
+          externalId: '123',
+          firstName: 'Duy',
+          lastName: 'Nguyen'
+        }
+      }
+    };
+    useQuerySpy.mockReturnValue(mockQueryValue as any);
+
+    render(
+      <AuthProvider {...oidcConfig}>
+        <MemoryRouter initialEntries={['/community/accounts']}>
+          <App />
+        </MemoryRouter>
+      </AuthProvider>
+    );
+
+    await waitFor(() => {
+      const welcomeTextOnAccountsScreen = screen.queryByText('Your Community');
+      expect(welcomeTextOnAccountsScreen).toBeInTheDocument();
+
+      const userFirstLastName = screen.queryByText('Duy Nguyen');
+      expect(userFirstLastName).toBeInTheDocument();
+    });
+  });
+});
+
+// authorized user, click Create Community button to navigate to create community page
+describe('given authorized user, when navigating to community/accounts/create-community', () => {
+  it('should display /community/accounts/create-community screen', async () => {
     const useAuthSpy = vi.spyOn(Auth, 'useAuth');
     const useQuerySpy = vi.spyOn(ApolloClient, 'useQuery');
 
@@ -56,12 +100,14 @@ describe('given authorized user named Duy Nguyen, when navigating to community/a
       </AuthProvider>
     );
 
+    const createCommunityButton = screen.getByText('Create a Community...');
+    await act(async () => {
+      await createCommunityButton.click();
+    });
+
     await waitFor(() => {
-      const welcomeTextOnAccountsScreen = screen.queryByText('Your Community');
-      expect(welcomeTextOnAccountsScreen).toBeInTheDocument();
-      
-      const userFirstLastName = screen.queryByText('Duy Nguyen');
-      expect(userFirstLastName).toBeInTheDocument();
+      const createCommunityText = screen.queryByText('Creating your Community');
+      expect(createCommunityText).toBeInTheDocument();
     });
   });
 });

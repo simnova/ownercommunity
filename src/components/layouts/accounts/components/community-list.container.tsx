63fb6bd35a5b21a5dd6f887d3d04f68e55959707
Duy Nguyen <nkduy2010@gmail.com>
2024-02-05 17:12:24 -0500

    refactor

--- src/components/layouts/accounts/components/community-list.container.tsx
+++ src/components/layouts/accounts/components/community-list.container.tsx
@@ -1,34 +1,19 @@
-
 import { useQuery } from '@apollo/client';
 import { Community, CommunityListContainerCommunitiesQueryDocument } from '../../../../generated';
 import { CommunityList } from './community-list';
+import { ComponentQueryLoader } from '../../../ui/molecules/component-query-loader';
 
 export const CommunityListContainer: React.FC<any> = () => {
-
-  const { loading, error, data} = useQuery(CommunityListContainerCommunitiesQueryDocument);
-  
-  if(error){
-    return <>
-      <div>Error :( {JSON.stringify(error)}</div>
-    </>
-  } 
-
-  if(loading){
-    return <>
-      <div>Loading...</div>
-    </>
-  } 
-
-  if (typeof data === 'undefined' || typeof data.communities === 'undefined' || data.communities === null ) {
-    return <>
-      <div>No Data...</div>
-    </>
-  }
+  const { loading, error, data } = useQuery(CommunityListContainerCommunitiesQueryDocument);
 
   return (
-    <div>
-      <CommunityList data={{communities: data.communities as Community[]}} />
-    </div>
-  )
-  
-}
+    <ComponentQueryLoader
+      loading={loading}
+      hasData={data?.communities}
+      hasDataComponent={<CommunityList data={{ communities: data?.communities as Community[] }} />}
+      noDataComponent={<div>No Data...</div>}
+      error={error}
+      errorComponent={<div>Error :( {JSON.stringify(error)}</div>}
+    />
+  );
+};

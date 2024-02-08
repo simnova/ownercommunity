38e9de3f42c8c22ef46599459032995cc20fd6f0
Patrick Gidich <patrick.gidich@simnova.com>
2024-02-08 13:47:25 -0500

    add home page title to Breadcrumbs component

--- src/components/editor/components/breadcrumbs.test.tsx
+++ src/components/editor/components/breadcrumbs.test.tsx
@@ -1,10 +1,11 @@
 import { describe, test, expect, beforeEach } from 'vitest';
-import { Matcher, MatcherOptions, SelectorMatcherOptions, render} from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import { Breadcrumbs } from './breadcrumbs';
 import { MemoryRouter } from 'react-router-dom';
 import { Editor,Frame } from '@craftjs/core';
 import * as LocalData from '../page-layout';
 import { UnloadedPageLayout } from '../page-layout';
+import { s } from 'vitest/dist/reporters-1evA5lom.js';
 
 const usePageLayoutsSpy = vi.spyOn(LocalData, 'usePageLayouts');
 
@@ -19,19 +20,13 @@ describe('Given a nested path and page layouts are not loaded', async () => {
 
   describe('when rendering the breadcrumb component', async () => {  
     //Act
-    interface RenderContext {
-      container:HTMLElement;
-      getByTestId: (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement
-      getByText:  (id: Matcher, options?: SelectorMatcherOptions | undefined) => HTMLElement
-    }
-
-    beforeEach<RenderContext>(async (context) => {
+    beforeEach(async () => {
       usePageLayoutsSpy.mockReturnValue([
         givenPageLayouts,
         {} as any,
         {} as any
       ]);
-      const {container,getByTestId,getByText} = render(
+       render(
         <div data-testid="testHarness">
           <MemoryRouter initialEntries={[givenPath]}>
             <Editor resolver={{Breadcrumbs}} enabled={true}>
@@ -42,15 +37,11 @@ describe('Given a nested path and page layouts are not loaded', async () => {
           </MemoryRouter>
         </div>
       );
-      context.getByText = getByText;
-      context.container = container;
-      context.getByTestId = getByTestId;
-      
     });
     
-    test<RenderContext>('then I expect the component should not render',async  ({getByText}) => {
+    test('then I expect the component should not render',async  () => {
       //Assert
-      expect(getByText('Loading...')).not.toBeEmptyDOMElement();
+      expect(screen.getByText('Loading...')).not.toBeEmptyDOMElement();
     });
   });
 });
@@ -67,19 +58,13 @@ describe('Given a nested path', async () => {
 
   describe('when rendering the breadcrumb component', async () => {  
     //Act
-    interface RenderContext {
-      container:HTMLElement;
-      getByTestId: (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement
-      getByText:  (id: Matcher, options?: SelectorMatcherOptions | undefined) => HTMLElement
-    }
-
-    beforeEach<RenderContext>(async (context) => {
+    beforeEach(async () => {
       usePageLayoutsSpy.mockReturnValue([
         givenPageLayouts,
         {} as any,
         {} as any
       ]);
-      const {container,getByTestId,getByText} = render(
+      render(
         <div data-testid="testHarness">
           <MemoryRouter initialEntries={[givenPath]}>
             <Editor resolver={{Breadcrumbs}} enabled={true}>
@@ -90,22 +75,124 @@ describe('Given a nested path', async () => {
           </MemoryRouter>
         </div>
       );
-      context.getByText = getByText;
-      context.container = container;
-      context.getByTestId = getByTestId;
-      
     });
     
-    test<RenderContext>('then I expect the component should render',async  ({getByTestId}) => {
+    test('then I expect the component should render',async  () => {
       //Assert
-      expect(getByTestId('testHarness')).not.toBeEmptyDOMElement();
+      expect(screen.getByTestId('testHarness')).not.toBeEmptyDOMElement();
     });
 
-    test<RenderContext>('then I expect the component should match each element in the path',async  ({getByText}) => {
+    test('then I expect the component should match each element in the path',async  () => {
       //Assert
-      expect(getByText(givenPageLayouts[0].title)).not.toBeEmptyDOMElement();
-      expect(getByText(givenPageLayouts[1].title)).not.toBeEmptyDOMElement();
-      expect(getByText(givenPageLayouts[2].title)).not.toBeEmptyDOMElement();
+      expect(screen.getByRole('link', {name:givenPageLayouts[0].title})).not.toBeEmptyDOMElement();
+      expect(screen.getByRole('link', {name:givenPageLayouts[1].title})).not.toBeEmptyDOMElement();
+      expect(screen.getByRole('link', {name:givenPageLayouts[2].title})).not.toBeEmptyDOMElement();
+    });
+  });
+});
+
+
+describe('Given a nested path and home page title', async () => {
+  //Arrange
+  const givenHomePageTitle = 'MyHome';
+  const givenPath = '/about/learn-more';
+  const givenPageLayouts:LocalData.PageLayouts = [
+    {id:'1', parent:'ROOT', title:'Company Page',pageType:'pageType',path:'/',expanded:false,layout:'xxx' },
+    {id:'2', parent:'1', title:'about',pageType:'pageType',path:'/about',expanded:false,layout:'xxx' },
+    {id:'3', parent:'2', title:'learn more',pageType:'pageType',path:'/about/learn-more',expanded:false, layout:'' },
+  ] as LocalData.LoadedPageLayout[];
+
+  describe('when rendering the breadcrumb component and overriding homepage title', async () => {  
+    //Act
+    beforeEach(async () => {
+      usePageLayoutsSpy.mockReturnValue([
+        givenPageLayouts,
+        {} as any,
+        {} as any
+      ]);
+      render(
+        <div data-testid="testHarness">
+          <MemoryRouter initialEntries={[givenPath]}>
+            <Editor resolver={{Breadcrumbs}} enabled={true}>
+              <Frame>
+                <Breadcrumbs separator=">" homePageTitle={givenHomePageTitle} /> 
+              </Frame>
+            </Editor>
+          </MemoryRouter>
+        </div>
+      );
+    });
+    
+    test('then I expect the component should render',async  () => {
+      //Assert
+      expect(screen.getByTestId('testHarness')).not.toBeEmptyDOMElement();
+    });
+
+    test('then I expect the component should match each element in the path and override the home page title',async  () => {
+      //Assert
+      expect(screen.getByRole('link', {name:givenHomePageTitle})).not.toBeEmptyDOMElement();
+      expect(screen.getByRole('link', {name:givenPageLayouts[1].title})).not.toBeEmptyDOMElement();
+      expect(screen.getByRole('link', {name:givenPageLayouts[2].title})).not.toBeEmptyDOMElement();
+    });
+
+    test('then I expect the given page layouts not to be modified',async  () => {
+      //Assert
+      expect(givenPageLayouts[1].title).not.toBe(givenHomePageTitle);
+    });
+  });
+});
+
+
+describe('Given a nested path with dynamic segments', async () => {
+  //Arrange
+  const givenPath = '/units/627ad63f73ca0039b79248aa';
+  const givenPageLayouts:LocalData.PageLayouts = [
+    {id:'1', parent:'ROOT', title:'home',pageType:'pageType',path:'/',expanded:false,layout:'xxx' },
+    {id:'2', parent:'1', title:'about',pageType:'pageType',path:'/about',expanded:false,layout:'xxx' },
+    {id:'3', parent:'2', title:'learn more',pageType:'pageType',path:'/about/learn-more',expanded:false, layout:'xxx' },
+    {id:'4', parent:'3', title:'units',pageType:'List',path:'/units',expanded:false, layout:'xxx' },
+    {id:'5', parent:'4', title:'Details Page',pageType:'Details',path:'/units/:propertyId/*',expanded:false,layout:'xxx' },
+  ] as LocalData.LoadedPageLayout[];
+
+  describe('when rendering the breadcrumb component', async () => {  
+    //Act
+    beforeEach(async () => {
+      usePageLayoutsSpy.mockReturnValue([
+        givenPageLayouts,
+        {} as any,
+        {} as any
+      ]);
+      render(
+        <div data-testid="testHarness">
+          <MemoryRouter initialEntries={[givenPath]}>
+            <Editor resolver={{Breadcrumbs}} enabled={true}>
+              <Frame>
+                <Breadcrumbs separator=">" /> 
+              </Frame>
+            </Editor>
+          </MemoryRouter>
+        </div>
+      );
+    });
+    screen.debug();
+    
+    test('then I expect the component should render',async  () => {
+      //Assert
+      expect(screen.getByTestId('testHarness')).not.toBeEmptyDOMElement();
+    });
+
+    test('then I expect the breadcrumbs for each element in the path',async  () => {
+      //Assert
+      expect(screen.getByRole('link', {name:givenPageLayouts[0].title})).not.toBeEmptyDOMElement();
+      expect(screen.getByRole('link', {name:givenPageLayouts[3].title})).not.toBeEmptyDOMElement();
+      expect(screen.getByRole('link', {name:givenPageLayouts[4].title})).not.toBeEmptyDOMElement();
+    });
+
+    test('then I expect the breadcrumb links to should match each segment in the path',async  () => {
+      //Assert
+      expect(screen.getByRole('link', {name:givenPageLayouts[0].title}).getAttribute('href')).toEqual(givenPageLayouts[0].path);
+      expect(screen.getByRole('link', {name:givenPageLayouts[3].title}).getAttribute('href')).toEqual(givenPageLayouts[3].path);
+      expect(screen.getByRole('link', {name:givenPageLayouts[4].title}).getAttribute('href')).toEqual(givenPath);
     });
   });
 });
--- src/components/editor/components/breadcrumbs.tsx
+++ src/components/editor/components/breadcrumbs.tsx
@@ -1,18 +1,18 @@
 import { useEditor, useNode } from '@craftjs/core';
 import { Breadcrumb, Form, Input, theme } from 'antd';
 import { Link, useLocation } from 'react-router-dom';
-import { arePageLayoutsLoaded, usePageLayouts, getAncestors } from '../page-layout';
+import { arePageLayoutsLoaded, usePageLayouts, getAncestors, LoadedPageLayout } from '../page-layout';
 
 interface TextProp {
   separator: string;
+  homePageTitle?: string;
 }
 
-let Breadcrumbs:any;
-
-Breadcrumbs = ({ separator, ...props } : TextProp) => {
+const Breadcrumbs = ({ separator,homePageTitle, ...props } : TextProp) => {
   const {
     token: { colorTextBase, colorBgContainer }
   }=theme.useToken();
+
   const [pageLayouts] = usePageLayouts();
 
 
@@ -34,6 +34,16 @@ Breadcrumbs = ({ separator, ...props } : TextProp) => {
 
   const ancestors = getAncestors(pageLayouts, location.pathname);
 
+  const getTitle = (x:LoadedPageLayout, index:number) => {
+    const isHomePage:boolean = index === 0;
+    const shouldOverrideHomePageTitle:boolean = (homePageTitle !== null && homePageTitle !== undefined && homePageTitle.length > 0);
+
+    if(isHomePage && shouldOverrideHomePageTitle){
+      return homePageTitle;
+    }
+    return x.title;
+  }
+
   return (
     <div 
       className="px-4 py-2"
@@ -43,12 +53,12 @@ Breadcrumbs = ({ separator, ...props } : TextProp) => {
         <div role="listitem" className="cursor-pointer shadow rounded-lg p-8 relative" style={{
           backgroundColor: colorBgContainer,
         }}>
-          <Breadcrumb>{
+          <Breadcrumb separator={separator}>{
             ancestors.map(
-              (x:any) => {
+              (x:LoadedPageLayout, index:number) => {
               return (
                 <Breadcrumb.Item key={x.id}>
-                  <Link to={x.path} style={{color: colorTextBase}}>{x.title}</Link>
+                  <Link to={x.path} style={{color: colorTextBase}}>{ getTitle(x,index)}</Link>
                 </Breadcrumb.Item>
               ) 
             })
@@ -58,15 +68,19 @@ Breadcrumbs = ({ separator, ...props } : TextProp) => {
   )
 }
 
-var BreadcrumbsSettings = () => {
-  const { actions: { setProp}, separator  } = useNode((node) => ({  
-    separator: node.data.props.separator
+const BreadcrumbsSettings = () => {
+  const { actions: { setProp}, separator, homePageTitle  } = useNode((node) => ({  
+    separator: node.data.props.separator,
+    homePageTitle: node.data.props.homePageTitle
   }));
   return (
     <div>
       <Form layout="vertical">
         <Form.Item label="Separator">
           <Input placeholder="Separator" value={separator} onChange={(inputElement) => setProp((props:any) => props.title = inputElement.target.value)}  />
+        </Form.Item>  
+        <Form.Item label="Home Page Title">
+          <Input placeholder="HomePageTitle" value={homePageTitle} onChange={(inputElement) => setProp((props:any) => props.title = inputElement.target.value)}  />
         </Form.Item>  
       </Form>
     </div>
@@ -76,6 +90,7 @@ var BreadcrumbsSettings = () => {
 Breadcrumbs.craft = {
   props: {
     separator: '/',
+    homePageTitle: ''
   },
   related: {
     settings: BreadcrumbsSettings

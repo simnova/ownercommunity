c585c7fc71b40a7a388257d9c60a5f67a2fcf10d
Luka Specter <lukaspecter@gmail.com>
2024-02-12 13:40:19 -0500

    add otel logging + beta azureMonitor

--- graphql/otelFilter.ts
+++ graphql/otelFilter.ts
@@ -1,4 +1,5 @@
 import { Attributes, Context, Link, SpanKind, TraceFlags } from '@opentelemetry/api';
+import { SeverityNumber, logs } from '@opentelemetry/api-logs';
 import { ReadableSpan, Sampler, SamplingDecision, SamplingResult, Span, SpanProcessor } from '@opentelemetry/sdk-trace-base';
 
 // Currently samples nothing, regardless of
@@ -29,7 +30,19 @@ export class SpanFilteringProcessor implements SpanProcessor {
   onStart(_span: Span): void {}
 
   onEnd(span: ReadableSpan): void {
-    if (span.attributes['graphql.source']?.valueOf().toString().includes('serverDate')) {
+    const filter_word = 'serverDate';
+
+    if (span.attributes['graphql.source']?.valueOf().toString().includes(filter_word)) {
+      const logger = logs.getLogger('default');
+      logger.emit({
+        body: `${filter_word} span should be filtered`,
+        severityNumber: SeverityNumber.INFO,
+        severityText: 'INFO',
+        attributes: {
+          'log.type': 'LogRecord',
+        },
+      });
+
       // if (span.attributes['graphql.field.name']?.valueOf().toString().includes('property')) {
       span.attributes['ShouldThisBeFiltered?'] = 'YesItShould(SpanFilteringProcessor)';
       span.spanContext().traceFlags = TraceFlags.NONE; // Same as SamplingDecision.NOT_RECORD I think

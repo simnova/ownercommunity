fc55fb9ad55b8e203ce163a22a11bff93ec38476
Duy Nguyen <nkduy2010@gmail.com>
2024-02-12 09:37:41 -0500

    clean up

--- src/components/layouts/accounts/accounts.test.tsx
+++ src/components/layouts/accounts/accounts.test.tsx
@@ -14,8 +14,61 @@ import { Members } from '../members';
 // to be tested scenarios:
 // - not authorized user, render Not Authorized screen
 // - authorized user, render /community/accounts screen successfully with correct user's name, User Id
-// - authorized user, click Create Community button to navigate to create community page
-// - authorized user, click on a community to navigate to community detail screen
+// - authorized user, click Create Community button will navigate to create community page
+// - authorized user, click on a community will navigate to community detail screen
+
+const mockQueryValue = {
+  loading: false,
+  error: undefined,
+  data: {
+    userCurrent: {
+      externalId: '123',
+      firstName: 'Duy',
+      lastName: 'Nguyen'
+    },
+    communities: [
+      {
+        name: 'hello world 1',
+        domain: 'hello-world-1',
+        whiteLabelDomain: 'hello-world-1',
+        handle: 'hello-world-1',
+        publicContentBlobUrl: 'https://hello-world-1.blob.core.windows.net/public',
+        userIsAdmin: true,
+        id: 1,
+        schemaVersion: '1',
+        createdAt: '2021-08-25T00:00:00.000Z',
+        updatedAt: '2021-08-25T00:00:00.000Z',
+        __typename: 'Community'
+      },
+      {
+        name: 'hello world 2',
+        domain: 'hello-world-2',
+        whiteLabelDomain: 'hello-world-2',
+        handle: 'hello-world-2',
+        publicContentBlobUrl: 'https://hello-world-1.blob.core.windows.net/public',
+        userIsAdmin: true,
+        id: 2,
+        schemaVersion: '1',
+        createdAt: '2021-08-25T00:00:00.000Z',
+        updatedAt: '2021-08-25T00:00:00.000Z',
+        __typename: 'Community'
+      },
+      {
+        name: 'hello world 3',
+        domain: 'hello-world-3',
+        whiteLabelDomain: 'hello-world-3',
+        handle: 'hello-world-3',
+        publicContentBlobUrl: 'https://hello-world-1.blob.core.windows.net/public',
+        userIsAdmin: true,
+        id: 3,
+        schemaVersion: '1',
+        createdAt: '2021-08-25T00:00:00.000Z',
+        updatedAt: '2021-08-25T00:00:00.000Z',
+        __typename: 'Community'
+      }
+    ] as Community[]
+  }
+};
 
 // unauthorized user
 describe('given not authorized, when navigating to community/accounts', () => {
@@ -38,69 +91,16 @@ describe('given not authorized, when navigating to community/accounts', () => {
 // authorized user
 describe('given authorized user with name Duy Nguyen and user id 123,', () => {
   beforeEach(async () => {
-    // cleanup();
-    // vi.clearAllMocks();
     const useAuthSpy = vi.spyOn(Auth, 'useAuth');
     const mockResolveValue = {
       isAuthenticated: true
     };
     useAuthSpy.mockReturnValue(mockResolveValue as any);
 
-    const mockQueryValue = {
-      loading: false,
-      error: undefined,
-      data: {
-        userCurrent: {
-          externalId: '123',
-          firstName: 'Duy',
-          lastName: 'Nguyen'
-        },
-        communities: [
-          {
-            name: 'hello world 1',
-            domain: 'hello-world-1',
-            whiteLabelDomain: 'hello-world-1',
-            handle: 'hello-world-1',
-            publicContentBlobUrl: 'https://hello-world-1.blob.core.windows.net/public',
-            userIsAdmin: true,
-            id: 1,
-            schemaVersion: '1',
-            createdAt: '2021-08-25T00:00:00.000Z',
-            updatedAt: '2021-08-25T00:00:00.000Z',
-            __typename: 'Community'
-          },
-          {
-            name: 'hello world 2',
-            domain: 'hello-world-2',
-            whiteLabelDomain: 'hello-world-2',
-            handle: 'hello-world-2',
-            publicContentBlobUrl: 'https://hello-world-1.blob.core.windows.net/public',
-            userIsAdmin: true,
-            id: 2,
-            schemaVersion: '1',
-            createdAt: '2021-08-25T00:00:00.000Z',
-            updatedAt: '2021-08-25T00:00:00.000Z',
-            __typename: 'Community'
-          },
-          {
-            name: 'hello world 3',
-            domain: 'hello-world-3',
-            whiteLabelDomain: 'hello-world-3',
-            handle: 'hello-world-3',
-            publicContentBlobUrl: 'https://hello-world-1.blob.core.windows.net/public',
-            userIsAdmin: true,
-            id: 3,
-            schemaVersion: '1',
-            createdAt: '2021-08-25T00:00:00.000Z',
-            updatedAt: '2021-08-25T00:00:00.000Z',
-            __typename: 'Community'
-          }
-        ] as Community[]
-      }
-    };
+    
     const useQuerySpy = vi.spyOn(ApolloClient, 'useQuery');
-
     useQuerySpy.mockReturnValue(mockQueryValue as any);
+    
     await act(async () => {
       render(
         <AuthProvider {...oidcConfig}>

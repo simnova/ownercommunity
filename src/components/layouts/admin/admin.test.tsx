1e6ae5138b81a7961381695550f63d6b93347222
Reggie Easton <reggie.easton5@gmail.com>
2024-02-14 14:32:42 -0500

    Add role and permissions to admin test file

--- src/components/layouts/admin/admin.test.tsx
+++ src/components/layouts/admin/admin.test.tsx
@@ -24,7 +24,40 @@ const communityMockQueryValue = {
         id: '12345abcd',
         __typename: 'Role'
       }
-    ]
+    ],
+    role: 
+      {
+      roleName: 'Admin',
+      isDefault: 'true',
+      createdAt: '03/01/2024',
+      updatedAt: '03/01/2024',
+      id: '12345abcd',
+      __typename: 'Role',
+      permissions: 
+      {
+        serviceTicketPermissions: {
+          canCreateTickets: 'true',
+          canManageTickets: 'true',
+          canAssignTickets: 'true',
+          __typename: 'ServiceTicketPermissions'
+        },
+        communityPermissions: {
+          canManageRolesAndPermissions: 'true',
+          canManageCommunitySettings: 'true',
+          canManageSiteContent: 'true',
+          canManageMembers: 'true',
+          canEditOwnMemberProfile: 'true',
+          canEditOwnMemberAccounts: 'true',
+          __typename: 'CommunityPermissions'
+        },
+        propertyPermissions: {
+          canEditOwnProperty: 'true',
+          canManageProperties: 'true',
+          __typename: 'PropertyPermissions'
+        },
+        __typename: 'RolePermissions'
+      }
+    }
   }
 };
 
@@ -45,6 +78,9 @@ describe('given not authorized, when navigating to /community/admin', () => {
     });
   });
 });
+
+
+
 //authorized user admin homePage
 describe('given a community named Reggie Main Test Complex when navigating to /community/admin', () => {
   it('should display the community name Reggie Main Test Complex and correct community ID', async () => {
@@ -75,6 +111,8 @@ describe('given a community named Reggie Main Test Complex when navigating to /c
   });
 });
 
+
+
 //authorized user clicking roles tab and navigating to roles page
 describe('given a community named Reggie Main Test Complex when navigating to /community/admin/roles', () => {
   it('should display the roles page', async () => {
@@ -89,6 +127,8 @@ describe('given a community named Reggie Main Test Complex when navigating to /c
     const useQuerySpy = vi.spyOn(ApolloClient, 'useQuery');
 
     useQuerySpy.mockReturnValue(communityMockQueryValue as any);
+
+
     render(
       <AuthProvider {...oidcConfig}>
         <MemoryRouter initialEntries={['/community/12345abcd/admin']}>
@@ -96,7 +136,11 @@ describe('given a community named Reggie Main Test Complex when navigating to /c
         </MemoryRouter>
       </AuthProvider>
     );
+
+
     const rolesLink = screen.getByRole('link', { name: /roles/i });
+
+
     await act(async () => {
       rolesLink.click();
     });
@@ -107,7 +151,6 @@ describe('given a community named Reggie Main Test Complex when navigating to /c
 });
 
 //authorized user on roles page
-
 describe('given a community named Reggie Main Test Complex when on /community/admin/roles', () => {
   it('should display the roles page and correct roles list', async () => {
     // set up authenticated environment
@@ -122,7 +165,6 @@ describe('given a community named Reggie Main Test Complex when on /community/ad
 
     useQuerySpy.mockReturnValue(communityMockQueryValue as any);
 
-    // useQuerySpy.mockReturnValue(RoleMockQueryValue as any);
     await act(async () => {
       render(
         <AuthProvider {...oidcConfig}>
@@ -132,11 +174,89 @@ describe('given a community named Reggie Main Test Complex when on /community/ad
         </AuthProvider>
       );
     });
-    screen.logTestingPlaygroundURL();
 
     const isdefaultText = screen.getByRole('columnheader', {
       name: /is default/i
     });
     expect(isdefaultText).toBeInTheDocument();
+  });
+});
+
+
+
+//authorized user on roles page and clicking on a role to edit
+describe('given a community named Reggie Main Test Complex when on /community/admin/roles', () => {
+  it('should display the roles edit page after clicking the edit button', async () => {
+    // set up authenticated environment
+    const useAuthSpy = vi.spyOn(Auth, 'useAuth');
+    const mockResolveValue = {
+      isAuthenticated: true
+    };
+
+    useAuthSpy.mockReturnValue(mockResolveValue as any);
+
+    // set up user data from useQuery
+    const useQuerySpy = vi.spyOn(ApolloClient, 'useQuery');
+
+    useQuerySpy.mockReturnValue(communityMockQueryValue as any);
+
+
+    await act(async () => {
+      render(
+        <AuthProvider {...oidcConfig}>
+          <MemoryRouter initialEntries={['/community/12345abcd/admin/roles']}>
+            <App />
+          </MemoryRouter>
+        </AuthProvider>
+      );
+    });
+    
+
+    const editButton = screen.getByRole('button', {
+      name: /edit/i
+    });
+
+
+    await act(async () => {
+      editButton.click();
+    });
+    
+    const editRoleText = screen.getByText(/role detail/i)
+    expect(editRoleText).toBeInTheDocument();
+  });
+});
+
+//authorized user on roles edit page
+describe('given a community named Reggie Main Test Complex when on /community/admin/roles/12345abcd', () => {
+  it('should display the roles edit page and correct role details with all checkboxes checked', async () => {
+    // set up authenticated environment
+    const useAuthSpy = vi.spyOn(Auth, 'useAuth');
+    const mockResolveValue = {
+      isAuthenticated: true
+    };
+    useAuthSpy.mockReturnValue(mockResolveValue as any);
+
+    // set up user data from useQuery
+    const useQuerySpy = vi.spyOn(ApolloClient, 'useQuery');
+
+    useQuerySpy.mockReturnValue(communityMockQueryValue as any);
+
+    
+    await act(async () => {
+      render(
+        <AuthProvider {...oidcConfig}>
+          <MemoryRouter initialEntries={['/community/12345abcd/admin/roles/12345abcd']}>
+            <App />
+          </MemoryRouter>
+        </AuthProvider>
+      );
+    });
+
+    const roleDetailText = screen.getByText(/can edit own property/i);
+    expect(roleDetailText).toBeInTheDocument();
+    const checkbox = screen.getByRole('checkbox', {
+      name: /can edit own property/i
+    });
+    expect(checkbox).toBeChecked();
   });
 });

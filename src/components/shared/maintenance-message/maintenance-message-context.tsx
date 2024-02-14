0ce8bd367da372cc037d6d3168127494827eac45
Duy Nguyen <nkduy2010@gmail.com>
2024-02-14 17:17:59 -0500

    make maintenance component independent+add tests for it

--- src/components/shared/maintenance-message/maintenance-message-context.tsx
+++ src/components/shared/maintenance-message/maintenance-message-context.tsx
@@ -1,19 +1,27 @@
-import { createContext } from 'react'
+import { createContext } from 'react';
 
-export interface MaintenanceMessage {
-
-}
+export interface MaintenanceMessage {}
 
 export interface MaintenanceMessageInterface {
-  isMaintenance: boolean|undefined,
-  isImpending: boolean|undefined,
+  isMaintenance: boolean | undefined;
+  isImpending: boolean | undefined;
+  impendingMessage?: string;
+  maintenanceMessage?: string;
+  impendingStartTimestamp: string;
+  maintenanceStartTimestamp: string;
+  maintenanceEndTimestamp: string;
 }
 
 const initialContext = {
   isMaintenance: undefined,
   isImpending: undefined,
-}
+  impendingMessage: '',
+  maintenanceMessage: '',
+  impendingStartTimestamp: '',
+  maintenanceStartTimestamp: '',
+  maintenanceEndTimestamp: ''
+};
 
-const MaintenanceMessageContext = createContext<MaintenanceMessageInterface>(initialContext)
+const MaintenanceMessageContext = createContext<MaintenanceMessageInterface>(initialContext);
 
-export default MaintenanceMessageContext
+export default MaintenanceMessageContext;

5af42b92a64f9f2e2d94596a9580b1490a220179
Nick Noce <nnoce12@gmail.com>
2024-02-16 16:24:01 -0500

    Fixed issue with open telemetry instrumentation

--- graphql/schema/types/server-date.resolvers.ts
+++ graphql/schema/types/server-date.resolvers.ts
@@ -6,24 +6,23 @@ const serverDate: Resolvers = {
   Query: {
     serverDate: async (_parent, args, context) => {
 
+let spanId = trace.getActiveSpan().spanContext().spanId;
+let traceId = trace.getActiveSpan().spanContext().traceId;
+let traceFlags = trace.getActiveSpan().spanContext().traceFlags;
+let traceState = trace.getActiveSpan().spanContext().traceState;
+let traceParent = trace.getActiveSpan().spanContext().traceParent;
+let isRemote = trace.getActiveSpan().spanContext().isRemote;
+let traceStateString = trace.getActiveSpan().spanContext().traceStateString;
+let traceStateHeader = trace.getActiveSpan().spanContext().traceStateHeader;
 
-// let spanId = trace.getActiveSpan().spanContext().spanId;
-// let traceId = trace.getActiveSpan().spanContext().traceId;
-// let traceFlags = trace.getActiveSpan().spanContext().traceFlags;
-// let traceState = trace.getActiveSpan().spanContext().traceState;
-// let traceParent = trace.getActiveSpan().spanContext().traceParent;
-// let isRemote = trace.getActiveSpan().spanContext().isRemote;
-// let traceStateString = trace.getActiveSpan().spanContext().traceStateString;
-// let traceStateHeader = trace.getActiveSpan().spanContext().traceStateHeader;
-
-// console.log(`spanId: ${spanId}`);
-// console.log(`traceId: ${traceId}`);
-// console.log(`traceFlags: ${traceFlags}`);
-// console.log(`traceState: ${traceState}`);
-// console.log(`traceParent: ${traceParent}`);
-// console.log(`isRemote: ${isRemote}`);
-// console.log(`traceStateString: ${traceStateString}`);
-// console.log(`traceStateHeader: ${traceStateHeader}`);
+console.log(`spanId: ${spanId}`);
+console.log(`traceId: ${traceId}`);
+console.log(`traceFlags: ${traceFlags}`);
+console.log(`traceState: ${traceState}`);
+console.log(`traceParent: ${traceParent}`);
+console.log(`isRemote: ${isRemote}`);
+console.log(`traceStateString: ${traceStateString}`);
+console.log(`traceStateHeader: ${traceStateHeader}`);
 
       return (new Date()).toString();
     }

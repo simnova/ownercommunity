0bc8f39e68b66e7602c4ec706f6d36703d6929c6
Mohit Gupta <mgupta.83@gmail.com>
2024-03-13 14:26:43 -0400

    static database

--- features/support/actors.ts
+++ features/support/actors.ts
@@ -1,8 +1,6 @@
 import { Actor, Cast, Notepad, TakeNotes } from '@serenity-js/core';
 import { InteractWithTheDomain } from './domain/abilities/interactWithTheDomain';
 import { ReadOnlyContext } from '../../domain/infrastructure/execution-context';
-// import { DomainExecutionContext } from '../../domain/contexts/context';
-import { IMemoryDatabase, MemoryDatabase } from './domain/adapter/infrastructure/persistance/memory-database';
 import { v4 as uuidV4 } from 'uuid';
 
 interface NotepadType {
@@ -14,20 +12,15 @@ interface NotepadType {
 }
 
 export class Actors implements Cast {
-
-    private readonly database: IMemoryDatabase;    // <aggregateRoot, collection>
     constructor(
-        // private readonly domainExecutionContext: DomainExecutionContext
     ) {
-        this.database = new MemoryDatabase();
-        
     }
 
   prepare(actor: Actor): Actor {
     const [firstName, lastName] = actor.name.split("The");
     const externalId = uuidV4();
 
-    InteractWithTheDomain.using(ReadOnlyContext, this.database).actOnUser(async (repo) => {
+    InteractWithTheDomain.using(ReadOnlyContext).actOnUser(async (repo) => {
       const user = await repo.getNewInstance(externalId, firstName, lastName);
       await repo.save(user);
     });

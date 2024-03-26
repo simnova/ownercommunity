b8c174dbc2b62bf62d56b3e1cf7ecb83c9c412c0
Duy Nguyen <nkduy2010@gmail.com>
2024-03-26 09:40:27 -0400

    fix query role by id

--- graphql/data-sources/cosmos-db/roles.ts
+++ graphql/data-sources/cosmos-db/roles.ts
@@ -5,10 +5,11 @@ import { Context } from "../../context";
 import { CosmosDataSource } from "./cosmos-data-source";
 
 export class Roles extends CosmosDataSource<Role, Context> {
+
   async getRoleById(id: string): Promise<Role> {
-    return (
-      await this.findByFields({ id: id, community: this.context.community })
-    )?.[0];
+    const roles = await this.findByFields({ community: this.context.community })
+    const result = roles.find(role => role.id === id);
+    return result
   }
 
   async getRoles(): Promise<Role[]> {

d99278d85ebc7d185bda57422830270713c46088
Duy Nguyen <nkduy2010@gmail.com>
2024-04-08 15:45:09 -0400

    fix var name

--- features/step-definitions/duy.steps.ts
+++ features/step-definitions/duy.steps.ts
@@ -58,6 +58,6 @@ Then('{word} should have the {word} role in {word}', async function (memberName:
 Then('{actor} should be a member of {word} under member named {word}', async function (actor: Actor, communityName: string, memberName: string) {
   const user = await notes<NotepadType>().get('user').answeredBy(actor);
   const member = await (await MemberInDb(memberName)).answeredBy(actor);
-  const accounts = member.accounts.items.map((item) => item.user).filter((user) => user.externalId === user.externalId);
+  const accounts = member.accounts.items.map((item) => item.user).filter((u) => u.externalId === user.externalId);
   await actor.attemptsTo(Ensure.that(accounts.length, equals(1)));  
 });

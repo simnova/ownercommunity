d8cf2aa6ad387ce3f0361cbb0313fa0480e0e41a
Dhaval Heruwala <heruwala@gmail.com>
2024-04-15 11:09:46 -0400

    Docusaurus -  Fix configuration so that Edit button works properly (#32)
    Fixes #31

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/simnova/ownercommunity/tree/main/docusaurus/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/simnova/ownercommunity/tree/main/docusaurus/',
         },
         theme: {
           customCss: './src/css/custom.css',

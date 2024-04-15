ac6c41c5e8e41da1827c88a771c3410f5c3ebd31
Dhaval Heruwala <heruwala@gmail.com>
2024-04-15 12:09:40 -0400

    Update GitHub link in docusaurus.config.ts (#34)
    * Update GitHub link in docusaurus.config.ts
    
    * Merge remote-tracking branch 'origin/main' into hd-GitHub

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/simnova/ownercommunity',
           label: 'GitHub',
           position: 'right',
         },

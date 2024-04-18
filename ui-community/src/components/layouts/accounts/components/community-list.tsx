40be02260422bc5ce42a5205dedd12fe0b509a7a
Yashasvi Jain <yjain@ecfmg.org>
2024-04-18 14:03:25 -0400

    Redesigned the community list component as a table.

--- ui-community/src/components/layouts/accounts/components/community-list.tsx
+++ ui-community/src/components/layouts/accounts/components/community-list.tsx
@@ -1,6 +1,8 @@
-import { Button, Typography, Tabs } from 'antd';
+import { Button, Typography, Table, Dropdown, Space } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { Community, Member } from '../../../../generated';
+import { ColumnFilterItem } from 'antd/es/table/interface';
+import { DownOutlined } from '@ant-design/icons';
 
 const { Title } = Typography;
 
@@ -13,50 +15,77 @@ export interface CommunityListProps {
 
 export const CommunityList: React.FC<CommunityListProps> = (props) => {
   const navigate = useNavigate();
+
+  const columns = [
+    {
+      title: 'Community Name',
+      dataIndex: 'community',
+      key: 'community',
+      filters:
+        props.data?.communities?.map(
+          (community) =>
+            ({
+              text: community.name as string,
+              value: community.name as string
+            } as ColumnFilterItem)
+        ) ?? [],
+      filterMode: 'menu' as 'menu',
+      filterSearch: true,
+      onFilter: (value: boolean | React.Key, record: any) => record.community.startsWith(value) as boolean,
+      width: '30%'
+    },
+    {
+      title: 'Member Portal',
+      dataIndex: 'memberPortal',
+      key: 'memberPortal'
+    },
+    {
+      title: 'Admin Portal',
+      dataIndex: 'adminPortal',
+      key: 'adminPortal'
+    }
+  ];
+
   let items = props.data?.communities?.map((community: any, i: number) => ({
     key: community.id,
-    label: community.name,
-    children: (
-      <div style={{float: 'left'}}>
-        <div style={{float: 'left', marginRight: 20}}>
-          <Title level={5} style={{textAlign: 'center'}}>Member Portal</Title>
-          {props.data?.members[i]?.map((member: Member) => (
-            <div key={member.id}>
-                <div className="listofbuttons">
-                  <Button
-                    type="default"
-                    key={member.id}
-                    data-testid="community-list-button"
-                    style={{ width: '200px', marginBottom: '10px' }}
-                    onClick={() => navigate(`/community/${community.id}/member/${member.id}`)}
-                  >
-                    {member.memberName}
-                  </Button>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div style={{float: 'left'}}>
-          <Title level={5} style={{textAlign: 'center'}}>Admin Portal</Title>
-          {props.data?.members[i]?.map((member: Member) => (
-            <div key={member.id}>
-              {member.isAdmin && (
-                  <div className="listofbuttons">
-                    <Button
-                      type="default"
-                      key={member.id + '-admin'}
-                      data-testid="community-list-admin-button"
-                      style={{ width: '200px', marginBottom: '10px' }}
-                      onClick={() => navigate(`/community/${community.id}/admin/${member.id}`)}
-                    >
-                      {member.memberName}
-                    </Button>
-                  </div>
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
+    community: community.name,
+    memberPortal: (
+      <Dropdown
+        menu={{
+          items: props.data?.members[i]?.map((member: Member) => ({
+            key: member.id as string,
+            label: <a onClick={() => navigate(`/community/${community.id}/member/${member.id}`)}>{member.memberName}</a>
+          }))
+        }}
+      >
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            Member Portals
+            <DownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
+    ),
+    adminPortal: (
+      <Dropdown
+        menu={{
+          items: props.data?.members[i]
+            ?.filter((member) => member.isAdmin)
+            .map((member: Member) => ({
+              key: member.id as string,
+              label: (
+                <a onClick={() => navigate(`/community/${community.id}/member/${member.id}`)}>{member.memberName}</a>
+              )
+            }))
+        }}
+      >
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            Admin Portals
+            <DownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
     )
   }));
 
@@ -70,7 +99,14 @@ export const CommunityList: React.FC<CommunityListProps> = (props) => {
       </div>
       <div className="w-full p-5 mx-auto my-5 shadow-lg rounded-lg">
         {items.length > 0 ? (
-          <Tabs items={items} tabPosition="left" />
+          <Table
+            dataSource={items}
+            columns={columns}
+            sticky={{
+              offsetHeader: 0
+            }}
+            pagination={{ position: ['topRight'] }}
+          />
         ) : (
           <Title level={5} style={{ display: 'flex', justifyContent: 'center' }}>
             You currently don't have any communities. Please create a community using the button on the right or join

25862a4c739b4419179daf6d04dfad41b9f2710a
Yashasvi Jain <yjain@ecfmg.org>
2024-05-29 11:59:53 -0400

    Added Helmet title for member pages

--- ui-community/src/components/layouts/members/pages/properties-detail.tsx
+++ ui-community/src/components/layouts/members/pages/properties-detail.tsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route, Link, useLocation, matchRoutes, useNavigate } from 'react-router-dom';
 import { PageHeader } from '@ant-design/pro-layout';
 import { Col, Menu, Row } from 'antd';
@@ -6,36 +5,46 @@ import { ProfileOutlined, CompassOutlined, FileOutlined } from '@ant-design/icon
 import React from 'react';
 import { PropertiesGeneral } from './properties-general';
 
-
 import { SubPageLayout } from '../sub-page-layout';
 import { PropertiesListing } from '../../shared/pages/properties-listing';
 import { PropertiesLocation } from '../../shared/pages/properties-location';
-
+import { Helmet } from 'react-helmet-async';
 
 export const PropertiesDetail: React.FC<any> = (_props) => {
   const location = useLocation();
   const navigate = useNavigate();
 
   const pages = [
-    {id: "1", path:'community/:communityId/member/:userId/properties/:id/', title:'General', icon:<ProfileOutlined />},
-    {id: "2", path:'community/:communityId/member/:userId/properties/:id/listing/*', title:'Listing', icon:<FileOutlined />},
-    {id: "3", path:'community/:communityId/member/:userId/properties/:id/location/*', title:'Location', icon:<CompassOutlined />},
-  ]
+    {
+      id: '1',
+      path: 'community/:communityId/member/:userId/properties/:id/',
+      title: 'General',
+      icon: <ProfileOutlined />
+    },
+    {
+      id: '2',
+      path: 'community/:communityId/member/:userId/properties/:id/listing/*',
+      title: 'Listing',
+      icon: <FileOutlined />
+    },
+    {
+      id: '3',
+      path: 'community/:communityId/member/:userId/properties/:id/location/*',
+      title: 'Location',
+      icon: <CompassOutlined />
+    }
+  ];
 
-  const matchedPages = matchRoutes(pages,location)
-  const matchedIds = matchedPages ? matchedPages.map((x:any) => x.route.id.toString()) : [];
+  const matchedPages = matchRoutes(pages, location);
+  const matchedIds = matchedPages ? matchedPages.map((x: any) => x.route.id.toString()) : [];
 
   return (
-    <SubPageLayout
-      fixedHeader={false}
-      header={
-        <PageHeader 
-          title="Property Detail"
-          onBack={() => navigate('../')}
-        />}
-      >
-        <Row wrap={false}>
-          <Col flex="none">
+    <SubPageLayout fixedHeader={false} header={<PageHeader title="Property Detail" onBack={() => navigate('../')} />}>
+      <Helmet>
+        <title>Property Detail</title>
+      </Helmet>
+      <Row wrap={false}>
+        <Col flex="none">
           <Menu mode="inline" selectedKeys={matchedIds}>
             <Menu.Item key="1" icon={<ProfileOutlined />}>
               <Link to="">General</Link>
@@ -47,15 +56,15 @@ export const PropertiesDetail: React.FC<any> = (_props) => {
               <Link to="location">Location</Link>
             </Menu.Item>
           </Menu>
-          </Col>
-          <Col flex="auto" style={{paddingLeft:'24px'}}>
-            <Routes>
-              <Route path="" element={<PropertiesGeneral />} />
-              <Route path="/listing/*" element={<PropertiesListing />} />
-              <Route path="/location/*" element={<PropertiesLocation />} />
-            </Routes>
-          </Col>
-        </Row>
+        </Col>
+        <Col flex="auto" style={{ paddingLeft: '24px' }}>
+          <Routes>
+            <Route path="" element={<PropertiesGeneral />} />
+            <Route path="/listing/*" element={<PropertiesListing />} />
+            <Route path="/location/*" element={<PropertiesLocation />} />
+          </Routes>
+        </Col>
+      </Row>
     </SubPageLayout>
-  )
-}
+  );
+};

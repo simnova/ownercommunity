b21030d195cbbb97b4d35e4a8c46c0dbd782b3aa
Nick Noce <nnoce12@gmail.com>
2024-07-08 16:24:16 -0400

    Removed bad query on member data source

--- data-access/src/app/application-services/datastore/member.interface.ts
+++ data-access/src/app/application-services/datastore/member.interface.ts
@@ -1,7 +1,6 @@
 import { MemberData } from "../../external-dependencies/datastore";
 
 export interface MemberDatastoreApplicationService {
-  getMemberByCommunityIdUserId(communityId: string, userId: string): Promise<MemberData>;
   getMembers(): Promise<MemberData[]>;
   getMembersByCommunityId(communityId: string): Promise<MemberData[]>;
   getMembersAssignableToTickets(): Promise<MemberData[]>;

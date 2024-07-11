9530db4c99412d80f584658cccc578f4b041dfc6
Yashasvi Jain <yjain@ecfmg.org>
2024-07-11 14:58:49 -0400

    feat: add walet-customer-info page to the member side

---
+++ ui-community/src/components/layouts/shared/components/billing-info.container.tsx
@@ -0,0 +1,157 @@
+import { useEffect, useState } from 'react';
+import { BillingInfo } from './billing-info';
+import axios from 'axios';
+
+export interface WalletContainerProps {
+  data: any;
+}
+
+interface TokenOptions {
+  expirationMonth: string;
+  expirationYear: string;
+}
+
+type Callback = (err: any, token: string) => void;
+
+const configObject = {
+  authenticationType: 'http_signature',
+  runEnvironment: 'apitest.cybersource.com',
+  merchantID: 'ecfmg_faimer',
+  merchantKeyId: '15db87b6-5531-4771-9c19-cc34a2d435b9',
+  merchantsecretKey: 'iMt2CuZsRPcuURcM1L2VieNFmnqscxVR4/BPFFitn10=',
+  logConfiguration: {
+    enableLog: false
+  }
+};
+
+export const BillingInfoContainer: React.FC<WalletContainerProps> = ({ data }) => {
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [cardNumberValidationHelpText, setCardNumberValidationHelpText] = useState<string>('');
+  const [securityCodeValidationHelpText, setSecurityCodeValidationHelpText] = useState<string>('');
+  const [isCardContainerLoaded, setIsCardContainerLoaded] = useState(false);
+  const [flexMicroform, setFlexMicroform] = useState<any>(null);
+
+  useEffect(() => {
+    if (isCardContainerLoaded) {
+      const microformScript = document.createElement('script');
+
+      microformScript.src = 'https://flex.cybersource.com/cybersource/assets/microform/0.11/flex-microform.min.js';
+
+      microformScript.onload = async () => {
+        await axios
+          .get(`http://localhost:7071/api/cybersource/generate-key`)
+          .then((response) => {
+            createFlexObj(response.data.keyId, async function (data: any, field: string) {
+              switch (field) {
+                case 'number':
+                  if (data.valid === true) {
+                    setCardNumberValidationHelpText('');
+                  }
+                  break;
+                case 'securityCode':
+                  if (data.valid === true) {
+                    setSecurityCodeValidationHelpText('');
+                  }
+                  break;
+              }
+            });
+
+            // Get Microform iFrames
+            const frames = document.getElementsByTagName('iframe');
+
+            // Set CSS on Microform iFrames
+            for (const element of frames) {
+              element.style.height = '30px';
+              element.style.border = '1px solid #d5d0da';
+              element.style.paddingLeft = '10px';
+            }
+          })
+          .catch((error) => {
+            console.log('MICROFORM ERROR', error);
+          });
+      };
+      document.body.appendChild(microformScript);
+    }
+  }, [isCardContainerLoaded]);
+
+  const onCardNumberContainerLoaded = () => {
+    setIsCardContainerLoaded(true);
+  };
+
+  const createFlexObj = (keyId: string, clearValidationText: (data: any, field: string) => void): void => {
+    try {
+      const flex = new (window as { [key: string]: any })['Flex'](keyId);
+
+      // Setup styles for flex iframe
+      const myStyles = {
+        input: {
+          'font-size': '14px',
+          'font-family': 'helvetica, tahoma, calibri, sans-serif',
+          color: '#555',
+          'line-height': '30px'
+        },
+        ':focus': { color: 'black' },
+        ':disabled': { cursor: 'not-allowed' },
+        valid: { color: '#3c763d' },
+        invalid: { color: '#a94442' },
+        '::placeholder': { color: '#A4A4A4' }
+      };
+      const microform = flex.microform({ styles: myStyles });
+      setFlexMicroform(microform);
+
+      // Create fields
+      const number = microform.createField('number', { placeholder: 'Enter card number' });
+      const securityCode = microform.createField('securityCode', { maxLength: 4, placeholder: '•••' });
+
+      // Add event listeners
+      number.on('change', function (data: any) {
+        clearValidationText(data, 'number');
+      });
+      securityCode.on('change', function (data: any) {
+        clearValidationText(data, 'securityCode');
+      });
+
+      // Check if the containers exist in the DOM
+      // const cardNumberContainer = document.getElementById('card-number-container');
+      // const securityCodeContainer = document.getElementById('securityCode-container');
+
+      // console.log('Card Number Container:', cardNumberContainer);
+      // console.log('Security Code Container:', securityCodeContainer);
+
+      // Load fields
+      number.load('#card-number-container');
+      securityCode.load('#securityCode-container');
+    } catch (error) {
+      console.log('MICROFORM ERROR', error);
+    }
+  };
+
+  const createToken = (expirationMonth: string, expirationYear: string, callBack: Callback): void => {
+    const options: TokenOptions = {
+      expirationMonth: expirationMonth,
+      expirationYear: expirationYear
+    };
+
+    flexMicroform.createToken(options, function (error: any, token: any) {
+      if (error) {
+        console.log('CREATE TOKEN ERROR', error);
+      } else {
+        console.log('TOKEN CREATED');
+      }
+      callBack(error, token);
+    });
+  };
+
+  return (
+    <BillingInfo
+      errorMessage={errorMessage}
+      onSetErrorMessage={setErrorMessage}
+      cardNumberValidationHelpText={cardNumberValidationHelpText}
+      setCardNumberValidationHelpText={setCardNumberValidationHelpText}
+      securityCodeValidationHelpText={securityCodeValidationHelpText}
+      setSecurityCodeValidationHelpText={setSecurityCodeValidationHelpText}
+      onCardNumberContainerLoaded={onCardNumberContainerLoaded}
+      createToken={createToken}
+    />
+  );
+};
---
+++ ui-community/src/components/layouts/shared/components/billing-info.tsx
@@ -0,0 +1,273 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Row, Col, Button, DatePicker } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import axios from 'axios';
+
+interface BillingInfoProps {
+  errorMessage: string;
+  onSetErrorMessage: Function;
+  cardNumberValidationHelpText: string;
+  setCardNumberValidationHelpText: React.Dispatch<React.SetStateAction<string>>;
+  securityCodeValidationHelpText: string;
+  setSecurityCodeValidationHelpText: React.Dispatch<React.SetStateAction<string>>;
+  onCardNumberContainerLoaded: () => void;
+  createToken: (expirationMonth: string, expirationYear: string, callBack: any) => void;
+}
+
+const MonthPicker = DatePicker.MonthPicker;
+dayjs.extend(customParseFormat);
+
+type MicroformError = {
+  correlationId: string;
+  details: MicroformDetails[];
+  informationLink: string;
+  message: string;
+  name: string;
+  reason: string; //CREATE_TOKEN_VALIDATION_SERVERSIDE / CREATE_TOKEN_VALIDATION_FIELDS
+};
+
+type MicroformDetails = {
+  location: string;
+  message: string;
+};
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 }
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 19 }
+  }
+};
+
+export const BillingInfo: React.FC<BillingInfoProps> = (props) => {
+  const [form] = Form.useForm();
+  const [expirationMonth, setExpirationMonth] = useState('');
+  const [expirationYear, setExpirationYear] = useState('');
+  const [paymentToken, setPaymentToken] = useState('');
+
+  useEffect(() => {
+    props.onCardNumberContainerLoaded();
+  }, []);
+
+  const resetErrorMessages = () => {
+    // reset any displayed errors
+    props.setCardNumberValidationHelpText('');
+    props.setSecurityCodeValidationHelpText('');
+  };
+
+  const onChangeExpYear = (date: dayjs.Dayjs | null) => {
+    if (date !== null) {
+      setExpirationMonth(date.format('M').padStart(2, '0'));
+      setExpirationYear(date.format('YYYY'));
+    } else {
+      setExpirationMonth('');
+      setExpirationYear('');
+    }
+  };
+
+  const processMicroformError = (error: MicroformError) => {
+    switch (error.reason) {
+      case 'CREATE_TOKEN_VALIDATION_SERVERSIDE': {
+        props.onSetErrorMessage('Payment failed due to expired token. Please refresh the screen and try again.');
+        break;
+      }
+      case 'CREATE_TOKEN_VALIDATION_FIELDS': {
+        // Credit Card and/or CVV is not valid
+        error.details.forEach((detail: MicroformDetails, _index: number) => {
+          if (detail.location === 'number') {
+            props.setCardNumberValidationHelpText('Please enter a valid card number.');
+          } else if (detail.location === 'securityCode') {
+            props.setSecurityCodeValidationHelpText('Please enter the security code as shown on the card.');
+          }
+        });
+        break;
+      }
+    }
+  };
+
+  const onFinish = () => {
+    // reset any displayed errors
+    resetErrorMessages();
+
+    // create token for customer card
+    props.createToken(expirationMonth, expirationYear, async function (error: any, token: any) {
+      if (error) {
+        const microformError: MicroformError = error;
+        processMicroformError(microformError);
+      }
+
+      form.validateFields().then((_values) => {
+        if (token) {
+          console.log('TOKEN', token);
+          setPaymentToken(token);
+        }
+      });
+    });
+  };
+
+  const onCreateCustomer = async () => {
+    if (paymentToken !== '') {
+      await axios
+        .post('http://localhost:7071/api/cybersource/create-customer', {
+          paymentToken: paymentToken
+        })
+        .then((response) => {
+          console.log('CREATE CUSTOMER RESPONSE', response);
+        })
+        .catch((error) => {
+          console.log('CREATE CUSTOMER ERROR', error);
+        });
+    }
+  };
+
+  const onGetCustomerPaymentInstruments = async () => {
+    await axios
+      .post('http://localhost:7071/api/cybersource/get-customer-payment-instruments', {
+        customerId: '1CD4C5EE92E27A57E063AF598E0ACEC6'
+      })
+      .then((response) => {
+        console.log('GET PAYMENT INSTRUMENTS RESPONSE', response);
+      })
+      .catch((error) => {
+        console.log('GET PAYMENT INSTRUMENTS ERROR', error);
+      });
+  };
+
+  const onAddCustomerPaymentInstrument = async () => {
+    await axios
+      .post('http://localhost:7071/api/cybersource/add-customer-payment-instrument', {
+        customerId: '1CD4C5EE92E27A57E063AF598E0ACEC6',
+        paymentToken: paymentToken
+      })
+      .then((response) => {
+        console.log('ADD PAYMENT INSTRUMENT RESPONSE', response);
+      })
+      .catch((error) => {
+        console.log('ADD PAYMENT INSTRUMENT ERROR', error);
+      });
+  };
+
+  const onDeleteCustomerPaymentInstrument = async () => {
+    await axios
+      .post('http://localhost:7071/api/cybersource/delete-customer-payment-instrument', {
+        customerId: '1CD4C5EE92E27A57E063AF598E0ACEC6',
+        paymentInstrumentId: '1CE627A481F7E175E063AF598E0AF73D'
+      })
+      .then((response) => {
+        console.log('DELETE PAYMENT INSTRUMENT RESPONSE', response);
+      })
+      .catch((error) => {
+        console.log('DELETE PAYMENT INSTRUMENT ERROR', error);
+      });
+  };
+
+  const onProcessPaymentWithPaymentInstrument = async () => {
+    await axios
+      .post('http://localhost:7071/api/cybersource/process-payment-with-payment-instrument', {
+        paymentInstrumentId: '1CC3816AF671FCC3E063AF598E0A5FA7'
+      })
+      .then((response) => {
+        console.log('PROCESS PAYMENT RESPONSE', response);
+      })
+      .catch((error) => {
+        console.log('PROCESS PAYMENT ERROR', error);
+      });
+  };
+
+  const onUpdateCustomerDefaultPaymentInstrument = async () => {
+    await axios
+      .post('http://localhost:7071/api/cybersource/update-customer-default-payment-instrument', {
+        customerId: '1CD4C5EE92E27A57E063AF598E0ACEC6',
+        paymentInstrumentId: '1CD4BF677E8742EAE063AF598E0AEF98'
+      })
+      .then((response) => {
+        console.log('UPDATE DEFAULT PAYMENT INSTRUMENT RESPONSE', response);
+      })
+      .catch((error) => {
+        console.log('UPDATE DEFAULT PAYMENT INSTRUMENT ERROR', error);
+      });
+  };
+
+  const ErrorMessage = () => {
+    return props.errorMessage ? (
+      <div style={{ textAlign: 'center', fontWeight: 'bold', color: 'red' }}>{props.errorMessage}</div>
+    ) : (
+      <></>
+    );
+  };
+
+  return (
+    <>
+      <ErrorMessage />
+      <Form form={form} name="basic" initialValues={{ remember: true }}>
+        <Form.Item
+          label={
+            <div>
+              <span style={{ color: 'red' }}>* </span>Card Number
+            </div>
+          }
+          name="cardNumber"
+          validateStatus="error" //error{red} validating{black} warning{yellow}
+          help={props.cardNumberValidationHelpText}
+          style={{ borderRadius: '6px' }}
+        >
+          <div id="card-number-container"></div>
+        </Form.Item>
+
+        <Form.Item
+          label={'Card Expiration Date:'}
+          required={true}
+          style={{ marginTop: '2px', width: '100%' }}
+          name="expirationMonthPicker"
+          rules={[{ required: true, message: 'Please enter the expiration date as shown on the card.' }]}
+        >
+          <MonthPicker
+            id="expirationMonthPicker"
+            name="expirationMonthPicker"
+            placeholder="Month-Year"
+            format="MM-YYYY"
+            onChange={(date: any) => onChangeExpYear(date)}
+            disabledDate={(current: any) => {
+              return current && current < dayjs().endOf('day').subtract(1, 'day');
+            }}
+          ></MonthPicker>
+        </Form.Item>
+
+        <Form.Item
+          label={
+            <div>
+              <span style={{ color: 'red' }}>* </span> Card Security Code
+            </div>
+          }
+          name="securityCode"
+          validateStatus="error"
+          help={props.securityCodeValidationHelpText}
+        >
+          <div id="securityCode-container"></div>
+        </Form.Item>
+
+        <Button type="primary" htmlType="submit" onClick={() => onFinish()}>
+          {'Save Card'}
+        </Button>
+
+        <Button onClick={() => onCreateCustomer()}>{'Create Customer'}</Button>
+
+        <Button onClick={() => onGetCustomerPaymentInstruments()}>{'Get Customer Payment Instruments'}</Button>
+
+        <Button onClick={() => onAddCustomerPaymentInstrument()}>{'Add Customer Payment Instrument'}</Button>
+
+        <Button onClick={() => onDeleteCustomerPaymentInstrument()}>{'Delete Customer Payment Instrument'}</Button>
+
+        <Button onClick={() => onProcessPaymentWithPaymentInstrument()}>{'Process Payment'}</Button>
+
+        <Button onClick={() => onUpdateCustomerDefaultPaymentInstrument()}>
+          {'Update Customer Default Payment Instrument'}
+        </Button>
+      </Form>
+    </>
+  );
+};

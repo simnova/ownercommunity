3055e6cc5fd206880dc677ac18c7b16cb4bb39ec
Jason Morais <jasonmorais02@gmail.com>
2024-07-24 16:24:01 -0400

    submission button finished

--- ui-community/src/components/editor/components/ahp-id-form-confirmation.tsx
+++ ui-community/src/components/editor/components/ahp-id-form-confirmation.tsx
@@ -6,10 +6,12 @@ import { TextThing } from './text-thing';
 import { FileUploadButton } from '../../layouts/shared/components/file-upload-button';
 import { useMutation } from '@apollo/client';
 import { AuthResult } from '../../ui/molecules/azure-upload';
-import { AhpIdFormCommunityPublicFileCreateAuthHeaderDocument, AhpIdFormCommunityPublicFileRemoveDocument } from '../../../generated';
+import {
+  AhpIdFormCommunityPublicFileCreateAuthHeaderDocument,
+  AhpIdFormCommunityPublicFileRemoveDocument
+} from '../../../generated';
 import Title from 'antd/es/typography/Title';
 import { DeleteFilled, FileTextOutlined } from '@ant-design/icons';
-import { func } from 'prop-types';
 import TextArea from 'antd/lib/input/TextArea';
 
 interface AhpIdFormConfirmationProps {
@@ -17,6 +19,7 @@ interface AhpIdFormConfirmationProps {
   blobPath: string;
   value: number;
   response: string;
+  submitted: boolean;
 }
 
 const communityId = '669ff09eae443cf6818a5bed';
@@ -33,7 +36,14 @@ AhpIdFormConfirmationTop.craft = {
       incomingNodes.every((incomingNode) => incomingNode.data.type === TextComponent || TextThing)
   }
 };
-const AhpIdFormConfirmation: any = ({ fileName, blobPath, response, value, ...props }: AhpIdFormConfirmationProps) => {
+const AhpIdFormConfirmation: any = ({
+  fileName,
+  blobPath,
+  response,
+  value,
+  submitted,
+  ...props
+}: AhpIdFormConfirmationProps) => {
   const {
     token: { colorBgContainer }
   } = theme.useToken();
@@ -45,7 +55,8 @@ const AhpIdFormConfirmation: any = ({ fileName, blobPath, response, value, ...pr
     fileName: state.data.props.fileName,
     blobPath: state.data.props.blobPath,
     value: state.data.props.value,
-    response: state.data.props.response
+    response: state.data.props.response,
+    submitted: state.data.props.submitted
   }));
 
   const handleRadioChange = (value: number) => {
@@ -106,6 +117,11 @@ const AhpIdFormConfirmation: any = ({ fileName, blobPath, response, value, ...pr
       });
   };
 
+  const submitReview = () => {
+    setProp((props: any) => {
+      props.submitted = true;
+    });
+  };
 
   const downloadFile = async () => {
     const blob = await (await fetch(blobPath)).blob();
@@ -123,7 +139,10 @@ const AhpIdFormConfirmation: any = ({ fileName, blobPath, response, value, ...pr
       <div
         style={{
           backgroundColor: 'white',
-          padding: '15px 25px',
+          paddingTop: '25px',
+          paddingBottom: '50px',
+          paddingLeft: '25px',
+          paddingRight: '25px',
           border: '1px solid black',
           textAlign: 'left'
         }}
@@ -134,7 +153,7 @@ const AhpIdFormConfirmation: any = ({ fileName, blobPath, response, value, ...pr
             marginBottom: 10
           }}
         >
-          Does your ID Form look Accurate?
+          {submitted ? 'ID Form Review: Completed' : 'Does your ID Form look Accurate?'}
         </Title>
         <div style={{ marginBottom: 10 }}>
           {fileName && blobPath ? (
@@ -145,26 +164,27 @@ const AhpIdFormConfirmation: any = ({ fileName, blobPath, response, value, ...pr
           ) : (
             <></>
           )}
-          <FileUploadButton
-            authorizeRequest={handleAuthorizeRequest}
-            blobPath={`https://ownercommunity.blob.core.windows.net/${communityId}`}
-            onRemoveRequested={() => {
-              throw new Error('Not implemented');
-            }}
-            permittedContentTypes={[
-              'image/jpeg',
-              'image/png',
-              'image/gif',
-              'text/plain',
-              'text/csv',
-              'application/json',
-              'application/pdf'
-            ]}
-            permittedExtensions={['jpg', 'jpeg', 'png', 'gif', 'txt', 'csv', 'json', 'pdf']}
-            maxFileSizeBytes={10 * 1024 * 1024} // 10MB,
-            maxWidthOrHeight={2048}
-          />
-          {fileName && blobPath ? (
+          {!submitted ? (
+            <FileUploadButton
+              authorizeRequest={handleAuthorizeRequest}
+              blobPath={`https://ownercommunity.blob.core.windows.net/${communityId}`}
+              permittedContentTypes={[
+                'image/jpeg',
+                'image/png',
+                'image/gif',
+                'text/plain',
+                'text/csv',
+                'application/json',
+                'application/pdf'
+              ]}
+              permittedExtensions={['jpg', 'jpeg', 'png', 'gif', 'txt', 'csv', 'json', 'pdf']}
+              maxFileSizeBytes={10 * 1024 * 1024} // 10MB,
+              maxWidthOrHeight={2048}
+            />
+          ) : (
+            <></>
+          )}
+          {fileName && blobPath && !submitted ? (
             <Button
               type="default"
               onClick={removeFile}
@@ -175,24 +195,35 @@ const AhpIdFormConfirmation: any = ({ fileName, blobPath, response, value, ...pr
           ) : (
             <></>
           )}
-          <div style={{marginTop: 10}}>
-            <Radio.Group value={value} onChange={(e: any) => handleRadioChange(e.target.value)}>
-              <Space direction="vertical">
-                <Radio value={1}>Yes, my ID form is accurate</Radio>
-                <Radio value={2}>No, it needs some corrections</Radio>
-                <TextArea
-                  onChange={(e: any) => handleInputChange(e.target.value)}
-                  style={{
-                    width: 400,
-                    marginLeft: 10
-                  }}
-                  value={response}
-                  autoSize
-                />
-              </Space>
-            </Radio.Group>
-          </div>
+          {!submitted ? (
+            <div style={{ marginTop: 10 }}>
+              <Radio.Group value={value} onChange={(e: any) => handleRadioChange(e.target.value)}>
+                <Space direction="vertical">
+                  <Radio value={1}>Yes, my ID form is accurate</Radio>
+                  <Radio value={2}>No, it needs some corrections</Radio>
+                  <TextArea
+                    onChange={(e: any) => handleInputChange(e.target.value)}
+                    style={{
+                      width: 400,
+                      marginLeft: 10
+                    }}
+                    value={response}
+                    autoSize
+                  />
+                </Space>
+              </Radio.Group>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
+        {!submitted ? (
+          <Button type="primary" style={{ float: 'right' }} onClick={submitReview}>
+            Complete Review
+          </Button>
+        ) : (
+          <></>
+        )}
       </div>
     </Container>
   );

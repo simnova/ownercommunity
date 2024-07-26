019b9928d42dd5e4eb004e3ed6cb8578e3521d9c
Jason Morais <jasonmorais02@gmail.com>
2024-07-26 10:52:09 -0400

    checkbox selection and setting of ebedded component and embdded tag working

--- ui-community/src/components/layouts/ahp-proof-of-concepts/pages/request-list-page/case-details-page/active-case-details-page/active-case/chat-messager.tsx
+++ ui-community/src/components/layouts/ahp-proof-of-concepts/pages/request-list-page/case-details-page/active-case-details-page/active-case/chat-messager.tsx
@@ -1,12 +1,29 @@
 import TextArea from 'antd/lib/input/TextArea';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { ChatComponentButton } from './chat-component-button';
-import { DollarOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { DollarOutlined, FilePdfOutlined } from '@ant-design/icons';
+import { Button, Tag } from 'antd';
 import { RequestFeedbackButton } from './request-feedback-button';
 
 interface ChatMessagerProps {}
 export const ChatMessager: FC<ChatMessagerProps> = () => {
+  const [message, setMessage] = useState('');
+  const [embedding, setEmbedding] = useState<JSX.Element | undefined>(undefined);
+  const [embeddingName, setEmbeddingName] = useState<string | undefined>(undefined);
+
+  const updateEmbedding = (embeddingName: string | undefined, embedding: JSX.Element | undefined) => {
+    setEmbedding(embedding);
+    setEmbeddingName(embeddingName);
+  };
+  const sendMessage = () => {
+    console.log('Heres the message: ', message, embedding);
+  };
+
+  const removeEmbed = () => {
+    setEmbedding(undefined);
+    setEmbeddingName(undefined);
+  };
+
   return (
     <div style={{ gridColumn: 1, border: '1px solid black', borderTop: '0px', width: '75%', display: 'flex' }}>
       <div
@@ -22,12 +39,18 @@ export const ChatMessager: FC<ChatMessagerProps> = () => {
         >
           Message
         </div>
+        {embedding && embeddingName && (
+          <Tag onClose={removeEmbed} closable>
+            <FilePdfOutlined /> {embeddingName}
+          </Tag>
+        )}
         <TextArea
           style={{ borderRadius: '0px' }}
           autoSize={{
             minRows: 3,
             maxRows: 5
           }}
+          onChange={(e: any) => setMessage(e.target.value)}
         />
         <div
           style={{
@@ -37,7 +60,7 @@ export const ChatMessager: FC<ChatMessagerProps> = () => {
             alignContent: 'stretch'
           }}
         >
-          <RequestFeedbackButton />
+          <RequestFeedbackButton updateEmbedding={updateEmbedding} />
           <ChatComponentButton modal={<></>} icon={<DollarOutlined />} text={'Request Payment'} />
           <ChatComponentButton modal={<></>} icon={<DollarOutlined />} text={'Send Money'} />
         </div>
@@ -47,6 +70,7 @@ export const ChatMessager: FC<ChatMessagerProps> = () => {
           marginTop: '40px',
           marginLeft: '10px'
         }}
+        onClick={sendMessage}
       >
         Send
       </Button>
--- ui-community/src/components/layouts/ahp-proof-of-concepts/pages/request-list-page/case-details-page/active-case-details-page/active-case/request-feedback-button.tsx
+++ ui-community/src/components/layouts/ahp-proof-of-concepts/pages/request-list-page/case-details-page/active-case-details-page/active-case/request-feedback-button.tsx
@@ -1,10 +1,14 @@
 import { FilePdfOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Modal } from 'antd';
 import { FC, useState } from 'react';
+import * as CmsComponents from '../../../../../../../editor/components';
 
-interface RequestFeedbackButtonProps {}
-export const RequestFeedbackButton: FC<RequestFeedbackButtonProps> = () => {
+interface RequestFeedbackButtonProps {
+  updateEmbedding: (embeddingName: string | undefined, embedding: JSX.Element | undefined) => void;
+}
+export const RequestFeedbackButton: FC<RequestFeedbackButtonProps> = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [checkedBoxes, setCheckedBoxes] = useState<string[]>([]);
 
   const openModal = () => {
     setIsOpen(true);
@@ -12,6 +16,44 @@ export const RequestFeedbackButton: FC<RequestFeedbackButtonProps> = () => {
 
   const closeModal = () => {
     setIsOpen(false);
+    if (checkedBoxes.length !== 0) {
+      switch (checkedBoxes[0]) {
+        case '1':
+          props.updateEmbedding('Request Updated Credential Type', <CmsComponents.AhpIdFormConfirmation />);
+          return;
+        case '2':
+          props.updateEmbedding('Request Updated Credential', <CmsComponents.AhpIdFormConfirmation />);
+          return;
+        case '3':
+          props.updateEmbedding('Request Updated Translation', <CmsComponents.AhpIdFormConfirmation />);
+          return;
+        case '4':
+          props.updateEmbedding('Request Updated Issuing Institution', <CmsComponents.AhpIdFormConfirmation />);
+          return;
+        case '5':
+          props.updateEmbedding('Request Updated Name on Document', <CmsComponents.AhpIdFormConfirmation />);
+          return;
+        default:
+          props.updateEmbedding('', undefined);
+          return;
+      }
+    }
+    props.updateEmbedding('', undefined);
+  };
+
+  const onCheck = (e: any) => {
+    console.log('checkboxdata', e.target.value, checkedBoxes);
+    if (checkedBoxes.length !== 0 && checkedBoxes.includes(e.target.value)) {
+      setCheckedBoxes([]);
+      return;
+    } else if (checkedBoxes.length !== 0) {
+      alert('Can only select one option.');
+      return;
+    }
+
+    let tempCheckedBoxes = checkedBoxes;
+    tempCheckedBoxes = [e.target.value];
+    setCheckedBoxes(tempCheckedBoxes);
   };
 
   return (
@@ -19,15 +61,26 @@ export const RequestFeedbackButton: FC<RequestFeedbackButtonProps> = () => {
       <Modal title="Request Update" open={isOpen} onCancel={closeModal} onOk={closeModal}>
         Request Applicant to:
         <br />
-        <Checkbox>add/update credential type</Checkbox>
+        <Checkbox value={1} onClick={onCheck} checked={checkedBoxes.includes('1')}>
+          {' '}
+          add/update credential type
+        </Checkbox>
         <br />
-        <Checkbox>add/update credential</Checkbox>
+        <Checkbox value={2} onClick={onCheck} checked={checkedBoxes.includes('2')}>
+          add/update credential
+        </Checkbox>
         <br />
-        <Checkbox>add/update credential translation</Checkbox>
+        <Checkbox value={3} onClick={onCheck} checked={checkedBoxes.includes('3')}>
+          add/update credential translation
+        </Checkbox>
         <br />
-        <Checkbox>add/update issuing institution</Checkbox>
+        <Checkbox value={4} onClick={onCheck} checked={checkedBoxes.includes('4')}>
+          add/update issuing institution
+        </Checkbox>
         <br />
-        <Checkbox>add/update name on document</Checkbox>
+        <Checkbox value={5} onClick={onCheck} checked={checkedBoxes.includes('5')}>
+          add/update name on document
+        </Checkbox>
       </Modal>
       <Button style={{ width: '100%', borderRadius: '0px' }} onClick={openModal}>
         <FilePdfOutlined /> Request Feedback

ff4d1b89348a7d02f90c530650e2d69d8584cc3b
Nick Noce <nnoce12@gmail.com>
2024-07-31 12:22:13 -0400

    Split user model into discriminators for end-user and staff-user

---
+++ data-access/src/infrastructure-services-impl/datastore/mongodb/models/users/end-user.ts
@@ -0,0 +1,59 @@
+import { Model, Schema } from "mongoose";
+import { User, UserModel, userOptions } from "./user";
+import { NestedPath, Patterns } from "../../../../../../seedwork/services-seedwork-datastore-mongodb/interfaces/base";
+
+export interface EndUserContactInformation extends NestedPath {
+  email: string;
+}
+
+export interface EndUserIdentityDetails extends NestedPath {
+  lastName: string;
+  legalNameConsistsOfOneName: boolean;
+  restOfName: string;
+}
+
+export interface EndUserPersonalInformation extends NestedPath {
+  identityDetails: EndUserIdentityDetails;
+  contactInformation: EndUserContactInformation;
+}
+
+export interface EndUser extends User {
+  personalInformation: EndUserPersonalInformation;
+}
+
+export const EndUserSchema = new Schema<EndUser, Model<EndUser>, EndUser>(
+  {
+    personalInformation: {
+      identityDetails: {
+        lastName: {
+          type: String,
+          required: true,
+          maxlength: 50,
+        },
+        legalNameConsistsOfOneName: {
+          type: Boolean,
+          required: true,
+          default: false,
+        },
+        restOfName: {
+          type: String,
+          required: false,
+          maxlength: 50,
+        },
+      },
+      contactInformation: {
+        email: {
+          type: String,
+          match: Patterns.EMAIL_PATTERN,
+          maxlength: 254,
+          unique: true,
+          required: true,
+          index: true,
+        },
+      },
+    },
+  },
+  userOptions
+);
+
+export const EndUserModel = UserModel.discriminator('end-users', EndUserSchema);

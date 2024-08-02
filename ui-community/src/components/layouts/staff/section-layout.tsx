4900eef814ae097ddba957ac6113d18b71b6eee9
ikeemwilson <IWilson@ECFMG.ORG>
2024-08-02 12:11:54 -0400

    Add logout button in staff section

--- ui-community/src/components/layouts/staff/section-layout.tsx
+++ ui-community/src/components/layouts/staff/section-layout.tsx
@@ -1,10 +1,27 @@
-import { Layout } from "antd";
+import {Avatar, Button, Layout, Space } from "antd";
 import { Outlet } from 'react-router-dom';
+import { useAuth } from 'react-oidc-context';
+import { GetUserRoles, UserRoles } from '../../../constants';
 
 export const SectionLayout: React.FC<any> = () => {
+  const auth = useAuth();
+  const userRoles = GetUserRoles();
+
+  const handleLogout = async () => {
+    await auth.removeUser();
+    await auth.signoutRedirect({ post_logout_redirect_uri: window.location.origin });
+  };
+  
   return (
-    <Layout className="site-layout" style={{ minHeight: '100vh' }}>
-      <Outlet />
+    <Layout className='site-layout' style={{ minHeight: '100vh' }}>
+      <div className='text-right ml-3'>
+        <Button style={{ margin: '5px 5px' }} onClick={() => handleLogout()}>Log Out</Button>
+      </div>
+      {userRoles.includes(UserRoles.Staff) ? (
+        <Outlet />
+      ) : (
+        <div>You are not a Staff user</div>
+      )}
     </Layout>
   )
 }

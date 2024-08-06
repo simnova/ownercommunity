497d3591b45e24f591820e5afc543a0e8d83ea25
Nick Noce <nnoce12@gmail.com>
2024-08-06 13:05:15 -0400

    Fixed role permissions on member to allow members to see their own role

--- data-access/src/app/application-services/roles/end-user-role/end-user-role.data.ts
+++ data-access/src/app/application-services/roles/end-user-role/end-user-role.data.ts
@@ -13,28 +13,28 @@ export class EndUserRoleDataApiImpl extends CosmosDataSource<EndUserRoleData, Ap
 
   async getRoleById(id: string): Promise<EndUserRoleData> {
     const roles = await this.findByFields({ community: this.context.community?.id });
-    const roleToReturn = roles.find(role => role.id === id);
-    if (roleToReturn && this.applyPermissions(roleToReturn)) {
-      return roleToReturn;
-    }
+    return roles.find(role => role.id === id);
+    // if (roleToReturn && this.applyPermissions(roleToReturn)) {
+    //   return roleToReturn;
+    // }
   }
 
   async getRoles(): Promise<EndUserRoleData[]> {
-    let rolesToReturn = await this.findByFields({ community: this.context.community?.id });
-    rolesToReturn = rolesToReturn.filter(roleData => this.applyPermissions(roleData));
-    return rolesToReturn;
+    return await this.findByFields({ community: this.context.community?.id });
+    // rolesToReturn = rolesToReturn.filter(roleData => this.applyPermissions(roleData));
+    // return rolesToReturn;
   }
 
   async getRolesByCommunityId(communityId: string): Promise<EndUserRoleData[]> {
-    let rolesToReturn = await this.findByFields({ community: communityId });
-    rolesToReturn = rolesToReturn.filter(roleData => this.applyPermissions(roleData));
-    return rolesToReturn;
+    return await this.findByFields({ community: communityId });
+    // rolesToReturn = rolesToReturn.filter(roleData => this.applyPermissions(roleData));
+    // return rolesToReturn;
   }
 
-  private applyPermissions(roleData: EndUserRoleData) {
-    if (this.context.passport.datastoreVisa.forEndUserRole(roleData).determineIf((permissions) => permissions.canManageStaffRolesAndPermissions || permissions.canManageRolesAndPermissions || permissions.isSystemAccount)) {
-      return true;
-    }
-    return false;
-  }
+  // private applyPermissions(roleData: EndUserRoleData) {
+  //   if (this.context.passport.datastoreVisa.forEndUserRole(roleData).determineIf((permissions) => permissions.canManageStaffRolesAndPermissions || permissions.canManageRolesAndPermissions || permissions.isSystemAccount)) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 }

a0c308e50677ca084ffa0fd16b0f3c5e5b0b2f7e
Shubham Joshi <shubham.joshi2812@gmail.com>
2024-08-12 22:15:54 +0530

    chore: Add error handling to ProcessGLTransactions function

--- data-access/src/routes/timer/gl-transaction/index.ts
+++ data-access/src/routes/timer/gl-transaction/index.ts
@@ -1,8 +1,13 @@
 import { TimerContext } from "../init/timer-context-builder";
 
 export const ProcessGLTransactions = async (context: TimerContext): Promise<void> => {
-  console.log('[TestTimerHandler] | Invocation ID ', context);
-  console.log('[TestTimerHandler] | Timer ', context);
-  // Timer function logic here
-  return;
+  try {
+    console.log('[TestTimerHandler] | Invocation ID ', context);
+    console.log('[TestTimerHandler] | Timer ', context);
+    // Timer function logic here
+    return;
+  } catch (error) {
+    console.error('[ProcessGLTransactions] | Error ', error);
+    throw error;
+  }
 }

a53ebc26eb05b11efff317de1981187e33f61cef
Mohit Gupta <mgupta.83@gmail.com>
2024-09-03 20:48:05 -0400

    wip

--- data-access/src/main/passport.ts
+++
@@ -1,7 +0,0 @@
-import { DatastoreVisa } from "../components/datastore/datastore.visa";
-import { DomainVisa } from "../components/domain/domain.visa";
-
-export interface Passport{
-    domainVisa: DomainVisa;
-    datastoreVisa: DatastoreVisa;
-}

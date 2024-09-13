23ccdc0d03b77c7dbfe36c9b19e9000387d64819
Nick Noce <nnoce12@gmail.com>
2024-09-13 15:43:44 -0400

    Setup timer job for integration test of blob service seedwork code

--- data-access/src/app/services/process-daily-gl-summaries.ts
+++ data-access/src/app/services/process-daily-gl-summaries.ts
@@ -20,25 +20,42 @@ export const ProcessDailyGLSummaries = async (blobSettings: BlobSettings) => {
   const startDate = await determineLastProcessedDate(blobSettings);
   const endDate = new Date();
 
-  const numberOfDays = getNumberOfDays(startDate, endDate);
-  for (let i = 0; i < numberOfDays; i++) {
-    const currentDate = new Date(startDate);
-    currentDate.setDate(currentDate.getDate() + i);
-    const transactionSummary: TransactionSummary[] = [
-      { transactionId: '1' },
-      { transactionId: '2' },
-      { transactionId: '3' },
-      { transactionId: '4' },
-    ];
-    const glPostingSummary: GlPostingSummary = {
-      glTransactionDate: currentDate,
-      glSummary: [
-        { accountingType: 'debit', amount: 100 },
-        { accountingType: 'credit', amount: 100 },
-        { accountingType: 'debit', amount: 100 },
-        { accountingType: 'credit', amount: 100 },
-      ]
-    };
-    await SaveDailyGlSummaries(currentDate, transactionSummary, glPostingSummary, blobSettings);
-  }
+  // const numberOfDays = getNumberOfDays(startDate, endDate);
+  // for (let i = 0; i < numberOfDays; i++) {
+  //   const currentDate = new Date(startDate);
+  //   currentDate.setDate(currentDate.getDate() + i);
+  //   const transactionSummary: TransactionSummary[] = [
+  //     { transactionId: '1' },
+  //     { transactionId: '2' },
+  //     { transactionId: '3' },
+  //     { transactionId: '4' },
+  //   ];
+  //   const glPostingSummary: GlPostingSummary = {
+  //     glTransactionDate: currentDate,
+  //     glSummary: [
+  //       { accountingType: 'debit', amount: 100 },
+  //       { accountingType: 'credit', amount: 100 },
+  //       { accountingType: 'debit', amount: 100 },
+  //       { accountingType: 'credit', amount: 100 },
+  //     ]
+  //   };
+  //   await SaveDailyGlSummaries(currentDate, transactionSummary, glPostingSummary, blobSettings);
+  // }
+
+  const transactionSummary: TransactionSummary[] = [
+    { transactionId: '1' },
+    { transactionId: '2' },
+    { transactionId: '3' },
+    { transactionId: '4' },
+  ];
+  const glPostingSummary: GlPostingSummary = {
+    glTransactionDate: startDate,
+    glSummary: [
+      { accountingType: 'debit', amount: 100 },
+      { accountingType: 'credit', amount: 100 },
+      { accountingType: 'debit', amount: 100 },
+      { accountingType: 'credit', amount: 100 },
+    ]
+  };
+await SaveDailyGlSummaries(startDate, transactionSummary, glPostingSummary, blobSettings);
 }
--- data-access/src/functions/timer/gl-transaction/index.ts
+++ data-access/src/functions/timer/gl-transaction/index.ts
@@ -1,10 +1,22 @@
 import { TimerContext } from "../init/timer-context-builder";
+import { ProcessDailyGLSummaries } from "../../../app/services/process-daily-gl-summaries";
 
 export const ProcessGLTransactions = async (context: TimerContext): Promise<void> => {
   try {
     console.log('[TestTimerHandler] | Invocation ID ', context);
     console.log('[TestTimerHandler] | Timer ', context);
     // Timer function logic here
+
+    const { blobStorage } = context.infrastructureServices;
+
+    const blobSettings = {
+      blobStorage: blobStorage,
+      blobContainerName: 'finance',
+      blobBasePath: 'daily-gl-summaries'
+    };
+
+    await ProcessDailyGLSummaries(blobSettings);
+
     return;
   } catch (error) {
     console.error('[ProcessGLTransactions] | Error ', error);

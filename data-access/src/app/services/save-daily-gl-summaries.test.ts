4bca2f8ce3c00c5abe3dd54eba950617666cb332
Nick Noce <nnoce12@gmail.com>
2024-09-19 11:28:16 -0400

    Added more unit tests

--- data-access/src/app/services/save-daily-gl-summaries.test.ts
+++ data-access/src/app/services/save-daily-gl-summaries.test.ts
@@ -1,4 +1,5 @@
-import { getNumberOfDays, sortDateArray } from "./save-daily-gl-summaries";
+import dayjs from "dayjs";
+import { sortDateArray, calculateStartDate, convertToStartOfDayEST } from "./save-daily-gl-summaries";
 
 describe('SaveDailyGlSummaries', () => {
   describe('sortDateArray', () => {
@@ -47,56 +48,117 @@ describe('SaveDailyGlSummaries', () => {
       expect(result).toEqual([]);
     });
   });
+  //   // scenario: no blob files exist in the blob storage, the determineLastProcessedDate function 
+  //   // returned the current date and we are comparing with a timestamp very close to that date
+  //   it('should return 1 when the start and end dates are one second apart', () => {
+  //     // Arrange
+  //     const startDate = new Date('2024-01-01Z23:59:58');
+  //     const endDate = new Date('2024-01-01Z23:59:59');
 
-  describe('getNumberOfDays', () => {
-    // scenario: no blob files exist in the blob storage, the determineLastProcessedDate function 
-    // returned the current date and we are comparing with a timestamp very close to that date
-    it('should return 1 when the start and end dates are one second apart', () => {
+  //     // Act
+  //     const result = getNumberOfDays(startDate, endDate);
+
+  //     // Assert
+  //     expect(result).toEqual(1);
+  //   });
+
+  //   it('should return 1 when the start and end dates are 1 day apart', () => {
+  //     // Arrange
+  //     const startDate = new Date('2024-01-01Z00:00:00');
+  //     const endDate = new Date('2024-01-02Z00:00:00');
+
+  //     // Act
+  //     const result = getNumberOfDays(startDate, endDate);
+
+  //     // Assert
+  //     expect(result).toEqual(1);
+  //   });
+
+  //   it('should return 2 when the start and end dates are more than 1 day apart', () => {
+  //     // Arrange
+  //     const startDate = new Date('2024-01-01Z00:00:00');
+  //     const endDate = new Date('2024-01-02Z0:00:01');
+
+  //     // Act
+  //     const result = getNumberOfDays(startDate, endDate);
+
+  //     // Assert
+  //     expect(result).toEqual(2);
+  //   });
+
+  //   it('should return 3 when the start and end dates are more than 2 days apart', () => {
+  //     // Arrange
+  //     const startDate = new Date('2024-09-11Z00:00:00');
+  //     const endDate = new Date('2024-09-13Z0:00:01');
+
+  //     // Act
+  //     const result = getNumberOfDays(startDate, endDate);
+
+  //     // Assert
+  //     expect(result).toEqual(3);
+  //   });
+  // });
+
+  describe('calculateStartDate', () => {
+    it('should return the current date minus one day when no blob dates are provided', () => {
       // Arrange
-      const startDate = new Date('2024-01-01Z23:59:58');
-      const endDate = new Date('2024-01-01Z23:59:59');
+      const blobDates: string[] = [];
 
       // Act
-      const result = getNumberOfDays(startDate, endDate);
+      const result = calculateStartDate(blobDates);
 
       // Assert
-      expect(result).toEqual(1);
+      expect(result).toEqual(new Date(Date.now() - 86400000));
     });
 
-    it('should return 1 when the start and end dates are 1 day apart', () => {
+    it('should return the last processed date plus one day when blob dates are provided', () => {
       // Arrange
-      const startDate = new Date('2024-01-01Z00:00:00');
-      const endDate = new Date('2024-01-02Z00:00:00');
+      const blobDates = [
+        '2024-01-01',
+        '2024-01-02',
+        '2024-01-03',
+      ];
 
       // Act
-      const result = getNumberOfDays(startDate, endDate);
+      const result = calculateStartDate(blobDates);
 
       // Assert
-      expect(result).toEqual(1);
+      expect(result).toEqual(new Date('2024-01-04'));
+    });
+  });
+
+  describe('convertToStartOfDayEST', () => {
+    it('should return null when the input date is null', () => {
+      // Arrange
+      const date = null;
+
+      // Act
+      const result = convertToStartOfDayEST(date);
+
+      // Assert
+      expect(result).toBeNull();
     });
 
-    it('should return 2 when the start and end dates are more than 1 day apart', () => {
+    it('should return the input date at the start of the day in EST time', () => {
       // Arrange
-      const startDate = new Date('2024-01-01Z00:00:00');
-      const endDate = new Date('2024-01-02Z0:00:01');
+      const date = new Date('2024-01-01T12:34:56Z');
 
       // Act
-      const result = getNumberOfDays(startDate, endDate);
+      const result = convertToStartOfDayEST(date);
 
       // Assert
-      expect(result).toEqual(2);
+      expect(result).toEqual(new Date('2024-01-01T05:00:00Z'));
     });
 
-    it('should return 3 when the start and end dates are more than 2 days apart', () => {
+    it('should return the input date at the start of the day in EST time', () => {
       // Arrange
-      const startDate = new Date('2024-09-11Z00:00:00');
-      const endDate = new Date('2024-09-13Z0:00:01');
+      const date = new Date('2024-09-18T17:30:13.247Z');
 
       // Act
-      const result = getNumberOfDays(startDate, endDate);
+      const result = convertToStartOfDayEST(date);
 
       // Assert
-      expect(result).toEqual(3);
+      expect(result).toEqual(new Date('2024-09-18T04:00:00Z'));
     });
   });
 

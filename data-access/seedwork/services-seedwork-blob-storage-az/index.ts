60f09a77959108743c4dc1c9b0c4d63aacdbb215
Nick Noce <nnoce12@gmail.com>
2024-10-02 09:41:29 -0400

    Updated docstring param

--- data-access/seedwork/services-seedwork-blob-storage-az/index.ts
+++ data-access/seedwork/services-seedwork-blob-storage-az/index.ts
@@ -44,9 +44,9 @@ export class AzBlobStorage implements BlobStorageBase {
   * @param blobName The name of the blob to create
    * @param containerName The name of the container to create the blob in
    * @param text The text to store in the blob
-   * @param callback The callback function which validates the file contents of the blob after upload
    * @param contentType (optional) The content type of the blob (default: text/plain)
    * @param tags (optional) The index tags to add to the blob
+   * @param callbackOnSuccess (optional) The callback function which validates the file contents of the blob after upload
    */
   public createTextBlobIfNotExistsAndConfirm(blobName: string, containerName: string, text: string, contentType:string='text/plain', tags?: Record<string, string>, callbackOnSuccess?: (blobText: string) => boolean):Promise<void>{
     return (new BlobActions(this.accountName,this.accountKey)).createTextBlobIfNotExistsAndConfirm(blobName,containerName,text,contentType,tags,callbackOnSuccess);

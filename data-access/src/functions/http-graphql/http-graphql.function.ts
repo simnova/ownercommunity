c4c2f33888220952e21e7cbaf4d8e3461ce4907c
Duy Nguyen <nkduy2010@gmail.com>
2024-12-19 15:00:37 -0600

    use new library to make apollo server and azure function v4 work together

--- data-access/src/functions/http-graphql/http-graphql.function.ts
+++ data-access/src/functions/http-graphql/http-graphql.function.ts
@@ -1,17 +1,17 @@
 import { wrapFunctionHandler } from '../core/telemetry/wrapper';
 
+import { v4 } from '@as-integrations/azure-functions';
 import { app } from '@azure/functions';
 import { PortalTokenValidation } from '../../../seedwork/auth-seedwork-oidc/portal-token-validation';
-import { GraphqlContextBuilder as ApolloContext } from './init/graphql-context-builder';
-import { startServerAndCreateHandler } from './init/func-v4'; // to be replaced by @as-integrations/azure-functions after PR is merged
 import { InfrastructureServicesBuilder } from '../../infrastructure-services-impl/infrastructure-services-builder';
-
+import { GraphqlContextBuilder as ApolloContext } from './init/graphql-context-builder';
 import { ApolloServerRequestHandler } from './init/apollo-server-request-handler';
+
 app.http('graphql', {
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
   route: 'graphql/{*segments}',
   handler: wrapFunctionHandler(
-    startServerAndCreateHandler(ApolloServerRequestHandler.getInstance().apolloServer, {
+    v4.startServerAndCreateHandler(ApolloServerRequestHandler.getInstance().apolloServer, {
       context: async ({ req }) => {
         let context = new ApolloContext(InfrastructureServicesBuilder.getInstance(), PortalTokenValidation.getInstance());
         await context.init(req);

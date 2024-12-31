5410c881c09a791e909f2a94d94f3a33dced66a8
dev-kishor <kishornathgupta1999@gmail.com>
2024-12-31 21:35:17 +0530

    Update Admin layout to include AdminPage

--- ui-community/src/components/layouts/admin/index.tsx
+++ ui-community/src/components/layouts/admin/index.tsx
@@ -1,12 +1,4 @@
-import {
-  BarsOutlined,
-  ContactsOutlined,
-  HomeOutlined,
-  LayoutOutlined,
-  SafetyOutlined,
-  ScheduleOutlined,
-  SettingOutlined
-} from '@ant-design/icons';
+import { BarsOutlined, ContactsOutlined, HomeOutlined, LayoutOutlined, SafetyOutlined, ScheduleOutlined, SettingOutlined } from '@ant-design/icons';
 import { Route, Routes, useParams } from 'react-router-dom';
 import { BlobToLocalStorage } from '../../shared/blob-to-local-storage';
 import { Home } from './pages/home';
@@ -18,6 +10,7 @@ import { Settings } from './pages/settings';
 import { SiteEditor } from './pages/site-editor';
 import { SectionLayoutContainer } from './section-layout.container';
 import { Member } from '../../../generated';
+import { AdminPage } from './pages/admin-page';
 
 export interface PageLayoutProps {
   path: string;
@@ -40,7 +33,7 @@ export const Admin: React.FC<any> = (_props) => {
     properties: 'properties/*',
     serviceTickets: 'service-tickets/*'
   };
-  
+
   const pageLayouts: PageLayoutProps[] = [
     { path: pathLocations.home, title: 'Home', icon: <HomeOutlined />, id: 'ROOT' },
     {
@@ -49,8 +42,7 @@ export const Admin: React.FC<any> = (_props) => {
       icon: <SettingOutlined />,
       id: 2,
       parent: 'ROOT',
-      hasPermissions: (member: Member) =>
-        member?.role?.permissions?.communityPermissions?.canManageCommunitySettings ?? false
+      hasPermissions: (member: Member) => member?.role?.permissions?.communityPermissions?.canManageCommunitySettings ?? false
     },
     {
       path: pathLocations.siteEditor,
@@ -66,8 +58,7 @@ export const Admin: React.FC<any> = (_props) => {
       icon: <SafetyOutlined />,
       id: 4,
       parent: 'ROOT',
-      hasPermissions: (member: Member) =>
-        member?.role?.permissions?.communityPermissions?.canManageRolesAndPermissions ?? false
+      hasPermissions: (member: Member) => member?.role?.permissions?.communityPermissions?.canManageRolesAndPermissions ?? false
     },
     {
       path: pathLocations.members,
@@ -91,8 +82,8 @@ export const Admin: React.FC<any> = (_props) => {
       icon: <ScheduleOutlined />,
       id: 7,
       parent: 'ROOT',
-      hasPermissions: (member: Member) => (member?.role?.permissions?.serviceTicketPermissions?.canManageTickets || 
-                                          member?.role?.permissions?.violationTicketPermissions?.canManageTickets) ?? false
+      hasPermissions: (member: Member) =>
+        (member?.role?.permissions?.serviceTicketPermissions?.canManageTickets || member?.role?.permissions?.violationTicketPermissions?.canManageTickets) ?? false
     }
   ];
 
@@ -100,13 +91,7 @@ export const Admin: React.FC<any> = (_props) => {
     <BlobToLocalStorage communityId={params.communityId}>
       <Routes>
         <Route path="" element={<SectionLayoutContainer pageLayouts={pageLayouts} />}>
-          <Route path={pathLocations.home} element={<Home />} />
-          <Route path={pathLocations.settings} element={<Settings />} />
-          <Route path={pathLocations.siteEditor} element={<SiteEditor />} />
-          <Route path={pathLocations.roles} element={<Roles />} />
-          <Route path={pathLocations.members} element={<Members />} />
-          <Route path={pathLocations.properties} element={<Properties />} />
-          <Route path={pathLocations.serviceTickets} element={<ServiceTickets />} />
+          <Route path="*" element={<AdminPage />} />
         </Route>
       </Routes>
     </BlobToLocalStorage>
